ed9edd3860b8f5754cc4dd13bd23719882f6ce53
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-08 10:10:30 +0200

    pair serializer

---
+++ internal-backend/src/main/java/org/exchange/app/backend/producers/PairSerializer.java
@@ -0,0 +1,29 @@
+package org.exchange.app.backend.producers;
+
+import exchange.app.internal.api.model.Pair;
+import java.util.Map;
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.common.serialization.Serializer;
+
+@Log4j2
+public class PairSerializer implements Serializer<Object> {
+
+  @Override
+  public byte[] serialize(String topic, Object data) {
+    if (data instanceof Pair pair) {
+      return pair.toString().getBytes();
+    } else {
+      throw new IllegalStateException("Can't serialize object: " + data);
+    }
+  }
+
+  @Override
+  public void configure(Map<String, ?> configs, boolean isKey) {
+
+  }
+
+  @Override
+  public void close() {
+
+  }
+}
