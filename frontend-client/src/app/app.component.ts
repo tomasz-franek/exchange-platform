6c7b33e0d1d256f42cf61d23626937031fc45575
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-14 09:56:51 +0200

    enable send order ticket button

--- frontend-client/src/app/app.component.ts
+++ frontend-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { OrderBookTableComponent } from './order-book-table/order-book-table.component';
+import { TicketOrderComponent } from './ticket-order/ticket-order.component';
 
 @Component({
   selector: 'app-root',
-  imports: [OrderBookTableComponent],
+  imports: [OrderBookTableComponent, TicketOrderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
