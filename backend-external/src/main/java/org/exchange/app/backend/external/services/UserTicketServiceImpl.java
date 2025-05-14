4fca23a1354d0652d3e21c664924ebe935f66e35
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-14 23:36:05 +0200

    refactor backend, adding backend-common

--- backend-external/src/main/java/org/exchange/app/backend/external/services/UserTicketServiceImpl.java
+++ backend-external/src/main/java/org/exchange/app/backend/external/services/UserTicketServiceImpl.java
@@ -1,17 +1,23 @@
 package org.exchange.app.backend.external.services;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.exchange.app.common.api.model.UserTicket;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
+@AllArgsConstructor
 public class UserTicketServiceImpl implements UserTicketService {
+
+  private final InputRecordProducer inputRecordProducer;
 
   @Override
   public void saveTicket(Long idUser, UserTicket userTicket) {
+
     log.info(userTicket);
+    inputRecordProducer.sendMessage(userTicket, userTicket.getPair());
   }
 
   @Override
