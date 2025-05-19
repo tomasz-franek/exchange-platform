e260f7a389377132927a82b948b8a752e30b5e1e
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-19 20:37:40 +0200

    remove kafka config from application classes

--- backend-external/src/main/java/org/exchange/app/backend/external/ExchangeExternalBackendApplication.java
+++ backend-external/src/main/java/org/exchange/app/backend/external/ExchangeExternalBackendApplication.java
@@ -1,23 +1,10 @@
 package org.exchange.app.backend.external;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.exchange.app.backend.common.config.CustomPartitioner;
-import org.exchange.app.backend.common.config.KafkaConfig;
-import org.exchange.app.backend.common.config.PairSerializer;
-import org.exchange.app.common.api.model.UserTicket;
-import org.exchange.app.common.api.model.Pair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @SpringBootApplication
 @EntityScan("org.exchange.app")
@@ -29,28 +16,5 @@ public class ExchangeExternalBackendApplication {
     app.setDefaultProperties(Collections
         .singletonMap("server.port", "8080"));
     app.run(args);
-  }
-
-  @Bean
-  public KafkaConfig kafkaConfig() {
-    return new KafkaConfig();
-  }
-
-  @Bean
-  public ProducerFactory<Pair, UserTicket> producerFactory() {
-    Map<String, Object> configProps = new HashMap<>();
-    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig().getBootstrapServers());
-    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, PairSerializer.class);
-    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-    configProps.put(JsonSerializer.TYPE_MAPPINGS,
-        "order:org.exchange.app.common.api.model.UserTicket");
-    configProps.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
-    return new DefaultKafkaProducerFactory<>(configProps);
-  }
-
-
-  @Bean
-  public KafkaTemplate<Pair, UserTicket> kafkaTemplate() {
-    return new KafkaTemplate<>(producerFactory());
   }
 }
--- backend-internal/src/main/java/org/exchange/app/backend/ExchangeConsumerBackendApplication.java
+++ backend-internal/src/main/java/org/exchange/app/backend/ExchangeConsumerBackendApplication.java
@@ -1,24 +1,10 @@
 package org.exchange.app.backend;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.exchange.app.backend.common.config.KafkaConfig;
-import org.exchange.app.common.api.model.Pair;
-import org.exchange.app.common.api.model.UserTicket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @SpringBootApplication
 @EntityScan("org.exchange.app")
@@ -32,37 +18,5 @@ public class ExchangeConsumerBackendApplication {
     app.run(args);
   }
 
-  @Bean
-  public ConsumerFactory<Pair, UserTicket> consumerFactory() {
-    return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-  }
 
-  @Bean
-  public KafkaConfig kafkaConfig() {
-    return new KafkaConfig();
-  }
-
-  @Bean
-  public Map<String, Object> consumerConfigs() {
-    Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "PLAINTEXT://kafka:9092");
-    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-    props.put(JsonDeserializer.TYPE_MAPPINGS,
-        "order:org.exchange.app.common.api.UserTicket");
-    return props;
-  }
-
-  @Bean
-  KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Pair, UserTicket>>
-  kafkaListenerContainerFactory() {
-    ConcurrentKafkaListenerContainerFactory<Pair, UserTicket> factory =
-        new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory());
-    factory.setChangeConsumerThreadName(true);
-    factory.setConcurrency(3);
-    factory.getContainerProperties().setPollTimeout(10);
-    factory.setBatchListener(true);
-    return factory;
-  }
 }
