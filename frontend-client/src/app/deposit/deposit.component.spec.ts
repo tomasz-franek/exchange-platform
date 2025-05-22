6fb095bc281e429d85b985eecce57a6c33a1d1ab
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-22 11:04:49 +0200

    tests for deposit component

--- frontend-client/src/app/deposit/deposit.component.spec.ts
+++ frontend-client/src/app/deposit/deposit.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DepositComponent } from './deposit.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { provideToastr } from 'ngx-toastr';
+import { provideMockStore } from '@ngrx/store/testing';
+import { initialTicketState } from '../state/tickets/ticket.reducers';
+import { TranslateTestingModule } from 'ngx-translate-testing';
+import assets_en from '../../assets/i18n/en.json';
+import assets_pl from '../../assets/i18n/pl.json';
+import { TranslateService } from '@ngx-translate/core';
+import { TicketOrderComponent } from '../ticket-order/ticket-order.component';
 
 describe('DepositComponent', () => {
   let component: DepositComponent;
@@ -8,9 +17,20 @@ describe('DepositComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DepositComponent]
-    })
-    .compileComponents();
+      imports: [
+        DepositComponent,
+        TranslateTestingModule.withTranslations(
+          'en',
+          assets_en,
+        ).withTranslations('pl', assets_pl),
+      ],
+      providers: [
+        FormBuilder,
+        ReactiveFormsModule,
+        provideToastr(),
+        provideMockStore({ initialState: initialTicketState }),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DepositComponent);
     component = fixture.componentInstance;
@@ -19,5 +39,70 @@ describe('DepositComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render page in english (default)', () => {
+    const translateService = TestBed.inject(TranslateService);
+    translateService.setDefaultLang('en');
+    const fixture = TestBed.createComponent(TicketOrderComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('button')?.textContent).toContain(
+      'Send order',
+    );
+  });
+
+  it('should render page in proper language', () => {
+    const fixture = TestBed.createComponent(TicketOrderComponent);
+
+    const translateService = TestBed.inject(TranslateService);
+    translateService.use('pl');
+
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('button')?.textContent).toContain(
+      'Wyślij zlecenie',
+    );
+  });
+
+  it('should have a form group with required fields', () => {
+    expect(component.formGroup.get('operation')).toBeTruthy();
+    expect(component.formGroup.get('value')).toBeTruthy();
+    expect(component.formGroup.get('currency')).toBeTruthy();
+  });
+
+  it('should validate operation field', () => {
+    const operationControl = component.formGroup.get('operation');
+    operationControl?.setValue('');
+    expect(operationControl?.valid).toBeFalse();
+    operationControl?.setValue('DEPOSIT');
+    expect(operationControl?.valid).toBeTrue();
+  });
+
+  it('should validate value field', () => {
+    const valueControl = component.formGroup.get('value');
+    valueControl?.setValue(0);
+    expect(valueControl?.valid).toBeFalse();
+    valueControl?.setValue(-20);
+    expect(valueControl?.valid).toBeFalse();
+    valueControl?.setValue(0.01);
+    expect(valueControl?.valid).toBeTrue();
+  });
+
+  it('should validate currency field', () => {
+    const currencyControl = component.formGroup.get('currency');
+    currencyControl?.setValue(null);
+    expect(currencyControl?.valid).toBeFalse();
+    currencyControl?.setValue('');
+    expect(currencyControl?.valid).toBeFalse();
+    currencyControl?.setValue('GBP');
+    expect(currencyControl?.valid).toBeTrue();
+  });
+
+  it('should validate form group', () => {
+    component.formGroup.get('value')?.setValue(0.01);
+    component.formGroup.get('currency')?.setValue('EUR');
+    component.formGroup.get('operation')?.setValue('WITHDRAW');
+    expect(component.formGroup.valid).toBeTrue();
   });
 });
