5b4ccc00f6a4405733090eb8f73387f929a1713e
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-25 20:42:14 +0200

    sidebar initial import

--- frontend-client/src/app/app.component.ts
+++ frontend-client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { TranslatePipe } from '@ngx-translate/core';
 import { AccountEditComponent } from './account-edit/account-edit.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AccountEditComponent],
+  imports: [TranslatePipe, AccountEditComponent],
 })
 export class AppComponent {
   title = 'frontend-client';
