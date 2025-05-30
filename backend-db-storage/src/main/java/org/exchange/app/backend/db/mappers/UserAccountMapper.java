687ba5e17969c5a36c0fa551b3ec44c7555f5b6a
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-30 08:35:14 +0200

    mappers for entities

---
+++ backend-db-storage/src/main/java/org/exchange/app/backend/db/mappers/UserAccountMapper.java
@@ -0,0 +1,36 @@
+package org.exchange.app.backend.db.mappers;
+
+import org.exchange.app.backend.db.entities.CurrencyEntity;
+import org.exchange.app.backend.db.entities.UserAccountEntity;
+import org.exchange.app.common.api.model.Currency;
+import org.exchange.app.common.api.model.UserAccount;
+import org.exchange.app.external.api.model.AccountBalance;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper()
+public interface UserAccountMapper {
+
+	UserAccountMapper INSTANCE = Mappers.getMapper(UserAccountMapper.class);
+
+
+	@Mapping(target = "currency", ignore = true)
+	UserAccountEntity toEntity(UserAccount userAccount);
+
+	@Mapping(target = "idUser", source = "userAccountEntity.user.id")
+	UserAccount toDto(UserAccountEntity userAccountEntity);
+
+	default Currency map(CurrencyEntity currencyEntity) {
+		return currencyEntity.getCode();
+	}
+
+	AccountBalance toAccountBalance(UserAccountEntity entity);
+
+	@Mapping(target = "id", ignore = true)
+	void updateWithDto(@MappingTarget UserAccountEntity entityToUpdate, UserAccount clinic);
+
+
+}
