5f50745cc4d895670a0d7aff3c2dc34c27dc579f
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-05-31 09:09:34 +0200

    tests preparing

---
+++ backend-external/src/test/java/org/exchange/app/backend/external/controllers/AccountsControllerTest.java
@@ -0,0 +1,64 @@
+package org.exchange.app.backend.external.controllers;
+
+import org.exchange.app.backend.db.repositories.UserAccountRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+class AccountsControllerTest {
+
+	@Autowired
+	private UserAccountRepository userAccountRepository;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	@Disabled
+	void updateUserAccount_when_notExistingCurrency_then_badRequest() throws Exception {
+//		mockMvc.perform(post("/accounts/user/account/{accountId}",
+//						"72aa8932-8798-4d1b-aaf0-590a3e6ffaa5").accept(APPLICATION_JSON).content("""
+//						{
+//							"idUserAccount":"72aa8932-8798-4d1b-aaf0-590a3e6ffa11",
+//							"idUser":"72aa8932-8798-4d1b-aaf0-590a3e6ffaa5",
+//							"idCurrency":999
+//						"""))
+//				.andExpect(status().isBadRequest())
+//				.andExpect(content().contentType(APPLICATION_JSON))
+//				.andExpect(jsonPath("$").isArray())
+//				.andExpect(jsonPath("$[0].idProduct").value(1))
+//				.andExpect(jsonPath("$[0].idShopping").value(1));
+	}
+
+	@Test
+	@Disabled
+	void updateUserAccount_when_notExistingUserId_then_badRequest() {
+
+	}
+
+	@Test
+	@Disabled
+	void updateUserAccount_when_correctDate_then_updatedResponse() {
+	}
+
+	@Test
+	@Disabled
+	void createUserAccount_when_wrongUserId_then_notFoundResponse() {
+	}
+
+	@Test
+	@Disabled
+	void createUserAccount_when_correctUserIdButExistingAccountCurrency_then_badRequest() {
+	}
+	@Test
+	@Disabled
+	void createUserAccount_when_correctUserIdAndNotExistingAccountCurrency_then_accountIsCreated() {
+	}
+}
