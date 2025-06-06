36c47d11455fd9226b2a1dade1fb98036a80648f
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-06-06 19:39:44 +0200

    new components

--- frontend-client/src/app/dashboard/dashboard.component.ts
+++ frontend-client/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { UserPropertyComponent } from '../user-property/user-property.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [UserPropertyComponent],
+  imports: [UserPropertyComponent, MenuComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
---
+++ frontend-client/src/app/menu/menu.component.ts
@@ -0,0 +1,48 @@
+import { Component, effect, inject } from '@angular/core';
+import {
+  HasRolesDirective,
+  KEYCLOAK_EVENT_SIGNAL,
+  KeycloakEventType,
+  ReadyArgs,
+  typeEventArgs,
+} from 'keycloak-angular';
+import { Router, RouterLink } from '@angular/router';
+import Keycloak from 'keycloak-js';
+
+@Component({
+  selector: 'app-menu',
+  imports: [HasRolesDirective, RouterLink],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent {
+  authenticated = false;
+  keycloakStatus: string | undefined;
+  private readonly keycloak = inject(Keycloak);
+  private readonly keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL);
+  private readonly router = inject(Router);
+
+  constructor() {
+    effect(() => {
+      const keycloakEvent = this.keycloakSignal();
+
+      this.keycloakStatus = keycloakEvent.type;
+
+      if (keycloakEvent.type === KeycloakEventType.Ready) {
+        this.authenticated = typeEventArgs<ReadyArgs>(keycloakEvent.args);
+      }
+
+      if (keycloakEvent.type === KeycloakEventType.AuthLogout) {
+        this.authenticated = false;
+      }
+    });
+  }
+
+  login() {
+    this.keycloak.login();
+  }
+
+  logout() {
+    this.keycloak.logout();
+  }
+}
