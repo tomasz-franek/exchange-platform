dce7b4be71239d09f57e1ae92422172884afc990
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-06-15 10:03:53 +0200

    allow expose dictionaries without authentication

--- backend-external/src/main/java/org/exchange/app/backend/external/keycloak/KeycloakConfiguration.java
+++ backend-external/src/main/java/org/exchange/app/backend/external/keycloak/KeycloakConfiguration.java
@@ -29,7 +29,8 @@ public class KeycloakConfiguration {
 
   private final static String[] allowedEndpoints = new String[]{
       "/swagger-ui/**",
-      "/v3/api-docs/**"
+      "/v3/api-docs/**",
+      "/dictionaries/**"
   };
 
   @Value("${exchange-portal.allowed-origins}")
