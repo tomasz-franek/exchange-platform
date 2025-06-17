455c698d4cc9a48935770246eb3aa5b12ef39f98
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-06-17 17:53:28 +0200

    keycloak config for frontend-admin

--- frontend-admin/src/app/app-routing.module.ts
+++ frontend-admin/src/app/app-routing.module.ts
@@ -1,17 +1,33 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {canActivateAuthAdminRole} from './services/auth-guard';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {ForbiddenComponent} from './forbidden/forbidden.component';
 
 const routes: Routes = [
   {
     path: 'accounts',
+    canActivate: [canActivateAuthAdminRole],
+    data: {role: 'EXCHANGE_ADMIN'},
     loadChildren: () =>
       import('./accounts/accounts.module').then(m => m.AccountsModule)
   },
   {
     path: 'transactions',
+    canActivate: [canActivateAuthAdminRole],
+    data: {role: 'EXCHANGE_ADMIN'},
     loadChildren: () =>
       import('./transactions/transactions.module').then(m => m.TransactionsModule)
-  }];
+  },
+  {
+    path: 'forbidden',
+    component: ForbiddenComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-admin/src/app/app.module.ts
+++ frontend-admin/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {ForbiddenComponent} from './forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +19,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
