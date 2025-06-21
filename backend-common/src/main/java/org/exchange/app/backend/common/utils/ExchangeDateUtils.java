6d14ad5723d481062ecee07aa5dd9a5c6693f570
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-06-21 13:06:21 +0200

    added missing imports

--- backend-common/src/main/java/org/exchange/app/backend/common/utils/ExchangeDateUtils.java
+++ backend-common/src/main/java/org/exchange/app/backend/common/utils/ExchangeDateUtils.java
@@ -9,32 +9,36 @@ import java.time.ZoneOffset;
 
 public class ExchangeDateUtils {
 
-	private static final ZoneId ZONE_UTC = ZoneOffset.UTC;
-	private static final Clock clock = Clock.systemUTC();
+  private static final ZoneId ZONE_UTC = ZoneOffset.UTC;
+  private static final Clock clock = Clock.systemUTC();
 
-	public static long toEpochUtc(LocalDateTime localDateTime) {
-		return localDateTime.toEpochSecond(ZoneOffset.UTC);
-	}
+  public static long toEpochUtc(LocalDateTime localDateTime) {
+    return localDateTime.toEpochSecond(ZoneOffset.UTC);
+  }
 
-	public static LocalDateTime toLocalDateTime(long epochUtc) {
-		return Instant.now(clock).atZone(ZONE_UTC).toLocalDateTime();
-	}
+  public static long toEpochUtc(Timestamp timestamp) {
+    return timestamp.toLocalDateTime().toEpochSecond(ZoneOffset.UTC);
+  }
 
-	public static Timestamp toTimestamp(long epochUtc) {
-		return Timestamp.valueOf(toLocalDateTime(epochUtc));
-	}
+  public static LocalDateTime toLocalDateTime(long epochUtc) {
+    return Instant.now(clock).atZone(ZONE_UTC).toLocalDateTime();
+  }
 
-	public static Timestamp currentTimestamp() {
-		return Timestamp.valueOf(
-				Instant.now().atZone(ZONE_UTC).toLocalDateTime());
-	}
+  public static Timestamp toTimestamp(long epochUtc) {
+    return Timestamp.valueOf(toLocalDateTime(epochUtc));
+  }
 
-	public static LocalDateTime currentLocalDateTime() {
-		return Instant.now().atZone(ZONE_UTC).toLocalDateTime();
-	}
+  public static Timestamp currentTimestamp() {
+    return Timestamp.valueOf(
+        Instant.now().atZone(ZONE_UTC).toLocalDateTime());
+  }
 
-	public static long currentEpochUtc() {
-		return clock.millis();
-	}
+  public static LocalDateTime currentLocalDateTime() {
+    return Instant.now().atZone(ZONE_UTC).toLocalDateTime();
+  }
+
+  public static long currentEpochUtc() {
+    return clock.millis();
+  }
 
 }
