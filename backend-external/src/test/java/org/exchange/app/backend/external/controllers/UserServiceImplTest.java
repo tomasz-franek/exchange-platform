8f17e162ff79775077d0d1de023bae65c47a7810
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-06-22 18:59:14 +0200

    preparing spring configuration for external tests

---
+++ backend-external/src/test/java/org/exchange/app/backend/external/controllers/UserServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.exchange.app.backend.external.controllers;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class UserServiceImplTest {
+
+  @Test
+  @Disabled
+  void saveUserProperty_should_createUserProperty_when_PropertiesAreNotCreated() {
+
+  }
+
+  @Test
+  @Disabled
+  void saveUserProperty_should_updateUserProperty_when_PropertiesAreCreated() {
+  }
+
+  @Test
+  @Disabled
+  void getUserProperty_should_shouldGetUserProperty_when_propertiesAreCreated() {
+  }
+
+  @Test
+  @Disabled
+  void getUserProperty_should_shouldReturnNotFound_when_propertiesAreCreated() throws Exception {
+  }
+}
