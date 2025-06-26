284a0c7febf0fc3956c4d0234f20135a33d3c9a5
Tomasz Franek <23628164+tomasz-franek@users.noreply.github.com>
2025-06-26 16:42:45 +0200

    new tests for currency utils (#63)

--- backend-common/src/main/java/org/exchange/app/backend/common/utils/CurrencyUtils.java
+++ backend-common/src/main/java/org/exchange/app/backend/common/utils/CurrencyUtils.java
@@ -2,12 +2,17 @@ package org.exchange.app.backend.common.utils;
 
 import static org.exchange.app.common.api.model.Direction.BUY;
 
+import jakarta.validation.constraints.NotNull;
 import org.exchange.app.common.api.model.Direction;
 import org.exchange.app.common.api.model.Pair;
 
 public class CurrencyUtils {
 
-  public static String pairToCurrency(final Pair currencyChange, final Direction direction) {
+  public static String pairToCurrency(final @NotNull Pair currencyChange,
+      final @NotNull Direction direction) {
+    if (currencyChange == null || direction == null) {
+      return "";
+    }
     if (BUY.equals(direction)) {
       return currencyChange.name().split("_")[1];
     } else {
@@ -15,7 +20,11 @@ public class CurrencyUtils {
     }
   }
 
-  public static String pairReverseCurrency(final Pair currencyChange, final Direction direction) {
+  public static String pairReverseCurrency(final @NotNull Pair currencyChange,
+      final @NotNull Direction direction) {
+    if (currencyChange == null || direction == null) {
+      return "";
+    }
     if (BUY.equals(direction)) {
       return currencyChange.name().split("_")[0];
     } else {
