f47ac3478d58ce95d52bc10cf0e1abadabca87de
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-07 12:19:25 +0200

    api for reports statistics and transactions used in frontend-admin

--- frontend-admin/scripts/generate-all-apis.ts
+++ frontend-admin/scripts/generate-all-apis.ts
@@ -4,3 +4,6 @@ execSync('npm run generate-common-api', {stdio: 'inherit'});
 execSync('npm run generate-users-api', {stdio: 'inherit'});
 execSync('npm run generate-admin-accounts-api', {stdio: 'inherit'});
 execSync('npm run generate-admin-users-api', {stdio: 'inherit'});
+execSync('npm run generate-admin-reports-api', {stdio: 'inherit'});
+execSync('npm run generate-admin-statistics-api', {stdio: 'inherit'});
+execSync('npm run generate-admin-transactions-api', {stdio: 'inherit'});
