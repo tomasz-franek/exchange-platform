80b97401eb9e59f2184bdd30f03a625dfa6d4449
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-09 07:59:38 +0200

    added selectTransactions to UI API

--- frontend-admin/src/app/services/api.service.ts
+++ frontend-admin/src/app/services/api.service.ts
@@ -9,6 +9,9 @@ import {AccountsReportResponse} from "../api/model/accountsReportResponse";
 import {AdminStatisticsService} from '../api/api/adminStatistics.service';
 import {UsersStatisticResponse} from "../api/model/usersStatisticResponse";
 import {UsersStatisticRequest} from '../api/model/usersStatisticRequest';
+import {AdminTransactionsService} from '../api/api/adminTransactions.service';
+import {SelectTransactionRequest} from '../api/model/selectTransactionRequest';
+import {Transaction} from '../api/model/models';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +20,7 @@ export class ApiService {
   private readonly adminAccountsService = inject(AdminAccountsService);
   private readonly adminReportsService = inject(AdminReportsService)
   private readonly adminStatisticsService = inject(AdminStatisticsService);
+  private readonly adminTransactionsService = inject(AdminTransactionsService);
 
   public loadAccounts(userAccountRequest: UserAccountRequest): Observable<UserAccount[]> {
     return this.adminAccountsService.loadAccounts(userAccountRequest)
@@ -28,5 +32,9 @@ export class ApiService {
 
   public loadUsersStatistic(usersStatisticRequest: UsersStatisticRequest): Observable<UsersStatisticResponse> {
     return this.adminStatisticsService.loadUsersStatistic(usersStatisticRequest);
+  }
+
+  public selectTransactions(selectTransactionRequest: SelectTransactionRequest): Observable<Transaction[]> {
+    return this.adminTransactionsService.selectTransactions(selectTransactionRequest);
   }
 }
