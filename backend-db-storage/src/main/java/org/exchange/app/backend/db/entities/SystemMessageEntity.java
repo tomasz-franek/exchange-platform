a5d1e3935d43d0b8d3b03174d6abbb5dbe5ee23b
Tomasz Franek <23628164+tomasz-franek@users.noreply.github.com>
2025-07-16 20:32:35 +0200

    structures for system messages (#83)

---
+++ backend-db-storage/src/main/java/org/exchange/app/backend/db/entities/SystemMessageEntity.java
@@ -0,0 +1,33 @@
+package org.exchange.app.backend.db.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import org.exchange.app.backend.db.DBConstants;
+
+@Entity
+@Table(name = "system_message", schema = DBConstants.SCHEMA_NAME)
+@Getter
+@Setter
+public class SystemMessageEntity {
+
+  @Id
+  private UUID id;
+
+  @Column(name = "create_date_utc", nullable = false)
+  LocalDateTime createDateUtc;
+
+  @Column(name = "message_text", nullable = false)
+  String messageText;
+
+  @Column(name = "active", nullable = false)
+  boolean active;
+
+  @Column(name = "priority", nullable = false)
+  short priority;
+}
