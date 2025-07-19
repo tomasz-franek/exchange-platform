3e745d45973c05e7f71b2f98cf02d3d21b3f3e78
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-19 09:09:14 +0200

    tests for transaction state admin

--- frontend-admin/src/app/transactions/state/transaction.reducers.spec.ts
+++ frontend-admin/src/app/transactions/state/transaction.reducers.spec.ts
@@ -1,10 +1,18 @@
 import {initialTransactionState, transactionReducers} from './transaction.reducers';
+import {selectTransactionsSuccess} from "./transaction.actions";
+import {Transaction} from "../../api/model/transaction";
 
 describe('transactionReducers', () => {
-  it('should return the initial state when no action is passed', () => {
-    const newState = transactionReducers(undefined, {
-      type: '',
-    });
-    expect(newState).toEqual(initialTransactionState);
+  it('should return the initial state', () => {
+    const action = {type: 'UNKNOWN_ACTION'}; // An action that does not affect the state
+    const state = transactionReducers(undefined, action);
+    expect(state).toBe(initialTransactionState);
+  });
+  it('should handle selectTransactionsSuccess', () => {
+    const transactions: Transaction[] = [{value: 10, dateUTC: 'x'}];
+    const action = selectTransactionsSuccess({transactions});
+    const state = transactionReducers(initialTransactionState, action);
+
+    expect(state.transactions).toEqual(transactions);
   });
 });
