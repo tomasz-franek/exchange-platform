5a45c541f6bff19dd27105de686a23c996a21f90
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-19 09:09:03 +0200

    tests for statistic state admin

--- frontend-admin/src/app/statistics/state/statistic.reducers.spec.ts
+++ frontend-admin/src/app/statistics/state/statistic.reducers.spec.ts
@@ -1,10 +1,22 @@
 import {initialStatisticState, statisticReducers} from './statistic.reducers';
+import {loadUserStatisticSuccess} from "./statistic.actions";
+import {UsersStatisticResponse} from "../../api/model/usersStatisticResponse";
 
 describe('statisticReducers', () => {
-  it('should return the initial state when no action is passed', () => {
-    const newState = statisticReducers(undefined, {
-      type: '',
-    });
-    expect(newState).toEqual(initialStatisticState);
+  it('should return the initial state', () => {
+    const action = {type: 'UNKNOWN_ACTION'}; // An action that does not affect the state
+    const state = statisticReducers(undefined, action);
+    expect(state).toBe(initialStatisticState);
+  });
+  it('should handle loadAccountListSuccess', () => {
+    const usersStatisticResponse: UsersStatisticResponse = {
+      active: 1,
+      all: 2,
+      blocked: 3
+    };
+    const action = loadUserStatisticSuccess({usersStatisticResponse});
+    const state = statisticReducers(initialStatisticState, action);
+
+    expect(state.usersStatisticResponse).toEqual(usersStatisticResponse);
   });
 });
