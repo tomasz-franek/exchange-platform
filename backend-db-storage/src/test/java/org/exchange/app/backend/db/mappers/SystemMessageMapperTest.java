ec43f22f21ec57b6372761bf6a8d8d2a1b2f70f0
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-25 06:32:20 +0200

    SystemMessageMapper tests

---
+++ backend-db-storage/src/test/java/org/exchange/app/backend/db/mappers/SystemMessageMapperTest.java
@@ -0,0 +1,77 @@
+package org.exchange.app.backend.db.mappers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.UUID;
+import org.exchange.app.backend.common.utils.ExchangeDateUtils;
+import org.exchange.app.backend.db.entities.SystemMessageEntity;
+import org.exchange.app.common.api.model.SystemMessage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+public class SystemMessageMapperTest {
+
+  private SystemMessageMapper mapper;
+
+  @BeforeEach
+  public void setUp() {
+    mapper = Mappers.getMapper(SystemMessageMapper.class);
+  }
+
+  @Test
+  public void toDto_should_copyEntityValues_when_methodCalled() {
+
+    SystemMessageEntity entity = new SystemMessageEntity();
+    entity.setId(UUID.randomUUID());
+    entity.setMessageText("Test message");
+    entity.setCreateDateUtc(ExchangeDateUtils.currentLocalDateTime());
+    entity.setVersion(1);
+    entity.setPriority((short) 4);
+
+    SystemMessage dto = mapper.toDto(entity);
+
+    assertEquals(entity.getId(), dto.getId());
+    assertEquals(entity.getMessageText(), dto.getMessageText());
+    assertEquals(entity.getVersion(), dto.getVersion());
+    assertEquals(entity.getPriority(), dto.getPriority());
+  }
+
+  @Test
+  public void toEntity_should_setMessageTextAndId_when_methodCalled() {
+
+    SystemMessage dto = new SystemMessage();
+    dto.setId(UUID.randomUUID());
+    dto.setMessageText("Test message");
+
+    SystemMessageEntity entity = mapper.toEntity(dto);
+
+    assertEquals(dto.getId(), entity.getId());
+    assertEquals(dto.getMessageText(), entity.getMessageText());
+    assertNull(entity.getCreateDateUtc());
+  }
+
+  @Test
+  public void updateWithDto_should_notUpdateId_when_methodCalled() {
+    UUID id = UUID.randomUUID();
+    SystemMessageEntity messageEntityToUpdate = new SystemMessageEntity();
+    messageEntityToUpdate.setId(id);
+    messageEntityToUpdate.setMessageText("Old message");
+    messageEntityToUpdate.setCreateDateUtc(ExchangeDateUtils.currentLocalDateTime());
+
+    SystemMessage systemMessage = new SystemMessage();
+    systemMessage.setMessageText("Updated message");
+    systemMessage.setId(UUID.randomUUID());
+
+    mapper.updateWithDto(messageEntityToUpdate, systemMessage);
+
+    assertThat(messageEntityToUpdate.getId()).isEqualTo(id);
+    assertEquals("Updated message", messageEntityToUpdate.getMessageText());
+
+    assertNotNull(messageEntityToUpdate.getCreateDateUtc());
+  }
+}
+
