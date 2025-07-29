89ae79a4e896bed855c2a14ca39244e88e468aa5
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-29 07:29:50 +0200

    new layout for forbidden page

--- frontend-admin/src/app/forbidden/forbidden.component.spec.ts
+++ frontend-admin/src/app/forbidden/forbidden.component.spec.ts
@@ -1,6 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ForbiddenComponent } from './forbidden.component';
+import {ForbiddenComponent} from './forbidden.component';
+import {TranslateService} from "@ngx-translate/core";
+import {TranslateTestingModule} from "ngx-translate-testing";
+import assets_en from "../../assets/i18n/en.json";
+import assets_pl from "../../assets/i18n/pl.json";
 
 describe('ForbiddenComponent', () => {
   let component: ForbiddenComponent;
@@ -8,8 +12,13 @@ describe('ForbiddenComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [ForbiddenComponent]
-})
+      imports: [ForbiddenComponent,
+        TranslateTestingModule.withTranslations(
+            'en',
+            assets_en,
+        ).withTranslations('pl', assets_pl),
+      ],
+    })
     .compileComponents();
 
     fixture = TestBed.createComponent(ForbiddenComponent);
@@ -19,5 +28,28 @@ describe('ForbiddenComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render page in english (default)', () => {
+    const translateService = TestBed.inject(TranslateService);
+    translateService.setDefaultLang('en');
+    const fixture = TestBed.createComponent(ForbiddenComponent);
+
+    fixture.detectChanges();
+    const idElement: HTMLElement =
+        fixture.nativeElement.querySelector('#forbidden');
+    expect(idElement.innerText).toContain('403 - Forbidden.');
+  });
+
+  it('should render page in proper language', () => {
+    const fixture = TestBed.createComponent(ForbiddenComponent);
+
+    const translateService = TestBed.inject(TranslateService);
+    translateService.use('pl');
+
+    fixture.detectChanges();
+    const idElement: HTMLElement =
+        fixture.nativeElement.querySelector('#forbidden');
+    expect(idElement.innerText).toContain('403 - Brak dostępu.');
   });
 });
--- frontend-admin/src/app/forbidden/forbidden.component.ts
+++ frontend-admin/src/app/forbidden/forbidden.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
-    selector: 'app-forbidden',
-    templateUrl: './forbidden.component.html',
-    styleUrl: './forbidden.component.css'
+  selector: 'app-forbidden',
+  templateUrl: './forbidden.component.html',
+  imports: [
+    TranslatePipe
+  ],
+  styleUrl: './forbidden.component.css'
 })
 export class ForbiddenComponent {
 
