39eef4efc61bbb1cc75d860748ada244e00043db
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-30 11:10:08 +0200

    tests for amount_realized

--- backend-external/src/test/java/org/exchange/app/backend/external/controllers/TicketsControllerTest.java
+++ backend-external/src/test/java/org/exchange/app/backend/external/controllers/TicketsControllerTest.java
@@ -24,62 +24,62 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class TicketsControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
 
-	@MockitoBean
-	private AuthenticationFacade authenticationFacade;
+  @MockitoBean
+  private AuthenticationFacade authenticationFacade;
 
-	@BeforeEach
-	public void beforeEach() {
-		when(authenticationFacade.getUserUuid())
-				.thenReturn(UUID.fromString("00000000-0000-0000-0002-000000000001"));
-	}
+  @BeforeEach
+  public void beforeEach() {
+    when(authenticationFacade.getUserUuid())
+        .thenReturn(UUID.fromString("00000000-0000-0000-0002-000000000001"));
+  }
 
-	@Test
-	void loadUserTicketList_should_returnListOfTicketForUser_when_called()
-			throws Exception {
-		mockMvc.perform(get("/tickets/list")
-						.contentType(APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(APPLICATION_JSON))
-				.andExpect(jsonPath("$").isArray())
-				.andExpect(jsonPath("$", hasSize(equalTo(1))))
-				.andExpect(jsonPath("$[0].id").value(1))
-				.andExpect(jsonPath("$[0].amount").value(100000))
-				.andExpect(jsonPath("$[0].pair").value("EUR_USD"))
-				.andExpect(jsonPath("$[0].direction").value("BUY"))
-				.andExpect(jsonPath("$[0].userAccountId").value("72aa8932-8798-4d1b-aaf0-590a3e6ffa22"))
-				.andExpect(jsonPath("$[0].ticketStatus").value("NEW"))
-				.andExpect(jsonPath("$[0].ratio").value("10312"))
-				.andExpect(jsonPath("$[0].version").value("0"))
-				.andExpect(jsonPath("$[0].eventType").value("DEPOSIT"));
-	}
+  @Test
+  void loadUserTicketList_should_returnListOfTicketForUser_when_called()
+      throws Exception {
+    mockMvc.perform(get("/tickets/list")
+            .contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(APPLICATION_JSON))
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$", hasSize(equalTo(1))))
+        .andExpect(jsonPath("$[0].id").value(1))
+        .andExpect(jsonPath("$[0].amount").value(100000))
+        .andExpect(jsonPath("$[0].pair").value("EUR_USD"))
+        .andExpect(jsonPath("$[0].direction").value("BUY"))
+        .andExpect(jsonPath("$[0].userAccountId").value("72aa8932-8798-4d1b-aaf0-590a3e6ffa22"))
+        .andExpect(jsonPath("$[0].ticketStatus").value("NEW"))
+        .andExpect(jsonPath("$[0].ratio").value("10312"))
+        .andExpect(jsonPath("$[0].version").value("0"))
+        .andExpect(jsonPath("$[0].eventType").value("EXCHANGE"));
+  }
 
-	@Test
-	void saveUserTicket_should_returnNotFound_when_missingAccountWithCurrency()
-			throws Exception {
+  @Test
+  void saveUserTicket_should_returnNotFound_when_missingAccountWithCurrency()
+      throws Exception {
 
-		mockMvc.perform(post("/tickets/create")
-						.contentType(APPLICATION_JSON)
-						.content("""
-								{
-									"id": 2,
-									"amount": 43000,
-									"ratio": 290000,
-									"pair" : "EUR_CHF",
-									"epochUTC": 222,
-									"direction" : "BUY",
-									"ticketStatus" : "NEW",
-									"eventType": "EXCHANGE",
-									"version": 0
-								}
-								"""))
-				.andExpect(status().isNotFound())
-				.andExpect(content().contentType(APPLICATION_JSON))
-				.andExpect(jsonPath("$.errorCode").value("OBJECT_WITH_ID_NOT_FOUND"))
-				.andExpect(jsonPath("$.message").value(
-						"Object UserAccount with id=Not found account for currency CHF not found"));
-	}
+    mockMvc.perform(post("/tickets/create")
+            .contentType(APPLICATION_JSON)
+            .content("""
+                {
+                	"id": 2,
+                	"amount": 43000,
+                	"ratio": 290000,
+                	"pair" : "EUR_CHF",
+                	"epochUTC": 222,
+                	"direction" : "BUY",
+                	"ticketStatus" : "NEW",
+                	"eventType": "EXCHANGE",
+                	"version": 0
+                }
+                """))
+        .andExpect(status().isNotFound())
+        .andExpect(content().contentType(APPLICATION_JSON))
+        .andExpect(jsonPath("$.errorCode").value("OBJECT_WITH_ID_NOT_FOUND"))
+        .andExpect(jsonPath("$.message").value(
+            "Object UserAccount with id=Not found account for currency CHF not found"));
+  }
 }
---
+++ backend-internal/src/test/java/org/exchange/app/backend/listeners/ExchangeTicketListenerTest.java
@@ -0,0 +1,79 @@
+package org.exchange.app.backend.listeners;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.exchange.app.backend.common.utils.ExchangeDateUtils;
+import org.exchange.app.backend.db.entities.ExchangeEventEntity;
+import org.exchange.app.backend.db.repositories.ExchangeEventRepository;
+import org.exchange.app.common.api.model.Direction;
+import org.exchange.app.common.api.model.EventType;
+import org.exchange.app.common.api.model.Pair;
+import org.exchange.app.common.api.model.UserTicketStatus;
+import org.exchange.internal.app.core.builders.CoreTicket;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ExchangeTicketListenerTest {
+
+  @Autowired
+  private ExchangeEventRepository exchangeEventRepository;
+
+  @Autowired
+  private ExchangeTicketListener exchangeTicketListener;
+
+  @Test
+  void loadOrderBook_should_reduceTicketAmountByPartiallyRealizedAmount_when_ticketsAreReadFromDatabase() {
+    ExchangeEventEntity exchangeEventEntity = new ExchangeEventEntity();
+    exchangeEventEntity.setAmount(100_0000L);
+    exchangeEventEntity.setAmountRealized(12_5000L);
+    exchangeEventEntity.setPair(Pair.EUR_PLN);
+    exchangeEventEntity.setDirection("S");
+    exchangeEventEntity.setEventType(EventType.EXCHANGE);
+    exchangeEventEntity.setTicketStatus(UserTicketStatus.PARTIAL_REALIZED);
+    exchangeEventEntity.setDateUtc(ExchangeDateUtils.currentTimestamp());
+    exchangeEventEntity.setRatio(1_0100L);
+    exchangeEventEntity.setUserAccountId(UUID.fromString("72aa8932-8798-4d1b-aaf0-590a3e6ffa22"));
+
+    exchangeEventEntity = exchangeEventRepository.save(exchangeEventEntity);
+
+    exchangeTicketListener.loadOrderBook();
+
+    Optional<CoreTicket> coreTicketOptional = exchangeTicketListener.exchangeServiceConcurrentHashMap.get(
+        Pair.EUR_PLN).removeOrder(exchangeEventEntity.getId(), Direction.SELL);
+    assertThat(coreTicketOptional).isNotEmpty();
+    assertThat(coreTicketOptional.get().getAmount()).isEqualTo(
+        exchangeEventEntity.getAmount() - exchangeEventEntity.getAmountRealized());
+    exchangeEventRepository.delete(exchangeEventEntity);
+  }
+
+  @Test
+  void loadOrderBook_should_readTotalAmount_when_ticketNotPartiallyRealized() {
+    ExchangeEventEntity exchangeEventEntity = new ExchangeEventEntity();
+    exchangeEventEntity.setAmount(30_0000L);
+    exchangeEventEntity.setAmountRealized(0L);
+    exchangeEventEntity.setPair(Pair.EUR_PLN);
+    exchangeEventEntity.setDirection("S");
+    exchangeEventEntity.setEventType(EventType.EXCHANGE);
+    exchangeEventEntity.setTicketStatus(UserTicketStatus.PARTIAL_REALIZED);
+    exchangeEventEntity.setDateUtc(ExchangeDateUtils.currentTimestamp());
+    exchangeEventEntity.setRatio(1_0100L);
+    exchangeEventEntity.setUserAccountId(UUID.fromString("72aa8932-8798-4d1b-aaf0-590a3e6ffa22"));
+
+    exchangeEventEntity = exchangeEventRepository.save(exchangeEventEntity);
+
+    exchangeTicketListener.loadOrderBook();
+
+    Optional<CoreTicket> coreTicketOptional = exchangeTicketListener.exchangeServiceConcurrentHashMap.get(
+        Pair.EUR_PLN).getFirstBookTicket(Direction.SELL);
+    assertThat(coreTicketOptional).isNotEmpty();
+    assertThat(coreTicketOptional.get().getAmount()).isEqualTo(
+        exchangeEventEntity.getAmount());
+    exchangeEventRepository.delete(exchangeEventEntity);
+  }
+}
