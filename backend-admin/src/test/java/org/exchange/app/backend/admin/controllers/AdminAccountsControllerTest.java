b6531a622b2271bf3e981f0a00b7d7d9fbd5e29c
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-07-31 09:52:08 +0200

    embedded kafka for tests

--- backend-admin/src/test/java/org/exchange/app/backend/admin/controllers/AdminAccountsControllerTest.java
+++ backend-admin/src/test/java/org/exchange/app/backend/admin/controllers/AdminAccountsControllerTest.java
@@ -14,10 +14,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092",
+    "port=9092"})
 public class AdminAccountsControllerTest {
 
   @Autowired
--- backend-internal/src/test/java/org/exchange/app/backend/listeners/ExchangeTicketListenerTest.java
+++ backend-internal/src/test/java/org/exchange/app/backend/listeners/ExchangeTicketListenerTest.java
@@ -16,9 +16,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092",
+    "port=9092"})
 class ExchangeTicketListenerTest {
 
   @Autowired
