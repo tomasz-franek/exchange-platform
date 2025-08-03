45d11625fbb733081301caf3a61e2da2e15acd41
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-03 21:20:51 +0200

    generate actuator API for admin

--- frontend-admin/scripts/generate-all-apis.ts
+++ frontend-admin/scripts/generate-all-apis.ts
@@ -10,3 +10,6 @@ execSync('npm run generate-admin-reports-api', {stdio: 'inherit'});
 execSync('npm run generate-admin-statistics-api', {stdio: 'inherit'});
 execSync('npm run generate-admin-transactions-api', {stdio: 'inherit'});
 execSync('npm run generate-admin-messages-api', {stdio: 'inherit'});
+execSync('npm run generate-actuator-internal-api', {stdio: 'inherit'});
+execSync('npm run generate-actuator-admin-api', {stdio: 'inherit'});
+execSync('npm run generate-actuator-external-api', {stdio: 'inherit'});
