a65587b2a9a4c62574b112fa03927db28ebca6aa
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-04 08:42:40 +0200

    added missing tests for message.actions.spec.ts

--- frontend-admin/src/app/messages/state/message.actions.spec.ts
+++ frontend-admin/src/app/messages/state/message.actions.spec.ts
@@ -1,5 +1,10 @@
-import {saveSystemMessageAction} from "./message.actions";
+import {
+  saveSystemMessageAction,
+  saveSystemMessageFailure,
+  saveSystemMessageSuccess
+} from "./message.actions";
 import {SystemMessage} from "../../api/model/systemMessage";
+import {HttpErrorResponse} from '@angular/common/http';
 
 describe('Messages Actions', () => {
   describe('saveSystemMessageAction', () => {
@@ -13,6 +18,31 @@ describe('Messages Actions', () => {
       const action = saveSystemMessageAction({systemMessage});
       expect(action.type).toBe('[Message] Saving system message Action',);
       expect(action.systemMessage).toEqual(systemMessage);
+    });
+  });
+
+  describe('saveSystemMessageSuccess', () => {
+    it('should create an action for successful loading of System Message', () => {
+      const systemMessage: SystemMessage = {
+        messageText: "Hello World!",
+        priority: 1,
+        active: false,
+        version: 0,
+      };
+      const action = saveSystemMessageSuccess({systemMessage});
+
+      expect(action.type).toBe('[Message] Save system message Action success');
+      expect(action.systemMessage).toEqual(systemMessage);
+    });
+  });
+
+  describe('saveSystemMessageFailure', () => {
+    it('should create an action for failed loading of System Message', () => {
+      const errorResponse = new HttpErrorResponse({error: 'Error message', status: 404});
+      const action = saveSystemMessageFailure({errorResponse});
+
+      expect(action.type).toBe('[Message] Save system message Action failure');
+      expect(action.errorResponse).toEqual(errorResponse);
     });
   });
 });
