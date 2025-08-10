65c7b03e3be8d5a13f69fe6bbfe55eec0cc5e2a5
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-10 09:52:26 +0200

    tests for DictionariesController

---
+++ backend-admin/src/test/java/org/exchange/app/backend/admin/controllers/DictionariesControllerTest.java
@@ -0,0 +1,46 @@
+package org.exchange.app.backend.admin.controllers;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DictionariesControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void loadTimezoneList_should_returnListOfTimeZones_when_endpointIsCalled()
+			throws Exception {
+		mockMvc.perform(get("/dictionaries/timezones")
+						.contentType(APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(APPLICATION_JSON))
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(603))));
+	}
+
+	@Test
+	void loadUnicodeLocalesList_should_returnListOfUnicodeLocales_when_endpointIsCalled()
+			throws Exception {
+		mockMvc.perform(get("/dictionaries/unicodeLocales")
+						.contentType(APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(APPLICATION_JSON))
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$", hasSize(equalTo(249))));
+	}
+}
