45139f18ced2b6abf233cfcd83c53bc8f98a35a9
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-15 09:52:00 +0200

    correct effect name

--- frontend-admin/src/app/transactions/state/transaction.effects.spec.ts
+++ frontend-admin/src/app/transactions/state/transaction.effects.spec.ts
@@ -1,23 +1,22 @@
-import {TestBed} from '@angular/core/testing';
-import {provideMockActions} from '@ngrx/effects/testing';
-import {Actions} from '@ngrx/effects';
-import {TransactionEffects} from './transaction.effects';
-import {ApiService} from '../../../services/api.service';
-import {cold, hot} from 'jasmine-marbles';
-import {HttpErrorResponse} from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Actions } from '@ngrx/effects';
+import { TransactionEffects } from './transaction.effects';
+import { ApiService } from '../../../services/api.service';
+import { cold, hot } from 'jasmine-marbles';
+import { HttpErrorResponse } from '@angular/common/http';
 import {
   loadTransactionListAction,
   loadTransactionListFailure,
-  loadTransactionListSuccess
-} from "./transaction.actions";
-import {Transaction} from "../../api/model/transaction";
-import {SelectTransactionRequest} from "../../api/model/selectTransactionRequest";
+  loadTransactionListSuccess,
+} from './transaction.actions';
+import { Transaction } from '../../api/model/transaction';
+import { SelectTransactionRequest } from '../../api/model/selectTransactionRequest';
 
 describe('TransactionEffects', () => {
   let actions$: Actions;
   let effects: TransactionEffects;
   let apiService: jasmine.SpyObj<ApiService>;
-
 
   beforeEach(() => {
     const apiServiceSpy = jasmine.createSpyObj('ApiService', [
@@ -28,47 +27,48 @@ describe('TransactionEffects', () => {
       providers: [
         TransactionEffects,
         provideMockActions(() => actions$),
-        {provide: ApiService, useValue: apiServiceSpy},
+        { provide: ApiService, useValue: apiServiceSpy },
       ],
     });
 
     effects = TestBed.inject(TransactionEffects);
     apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
   });
-  describe('loadTransactionListAction', () => {
+  describe('loadUserTransactions$', () => {
     it('should return loadTransactionListSuccess on successful load', () => {
       const selectTransactionRequest: SelectTransactionRequest = {
-        dateFromUTC: '', dateToUTC: '',
+        dateFromUTC: '',
+        dateToUTC: '',
       };
-      const action = loadTransactionListAction({selectTransactionRequest});
+      const action = loadTransactionListAction({ selectTransactionRequest });
       const transactions = [] as Transaction[];
-      const completion = loadTransactionListSuccess({transactions});
+      const completion = loadTransactionListSuccess({ transactions });
 
-      actions$ = hot('-a-', {a: action});
-      const response = cold('-b|', {b: transactions});
+      actions$ = hot('-a-', { a: action });
+      const response = cold('-b|', { b: transactions });
       apiService.loadTransactionList.and.returnValue(response);
 
-      const expected = cold('--c', {c: completion});
-      expect(effects.selectUserTransactions$).toBeObservable(expected);
+      const expected = cold('--c', { c: completion });
+      expect(effects.loadUserTransactions$).toBeObservable(expected);
     });
 
     it('should return loadTransactionListFailure on error', () => {
       const selectTransactionRequest: SelectTransactionRequest = {
-        dateFromUTC: '', dateToUTC: '',
+        dateFromUTC: '',
+        dateToUTC: '',
       };
-      const action = loadTransactionListAction({selectTransactionRequest});
-      const errorResponse = new HttpErrorResponse({error: 'Error'});
+      const action = loadTransactionListAction({ selectTransactionRequest });
+      const errorResponse = new HttpErrorResponse({ error: 'Error' });
       const completion = loadTransactionListFailure({
         errorResponse,
       });
 
-      actions$ = hot('-a-', {a: action});
+      actions$ = hot('-a-', { a: action });
       const response = cold('-#', {}, errorResponse);
       apiService.loadTransactionList.and.returnValue(response);
 
-      const expected = cold('--c', {c: completion});
-      expect(effects.selectUserTransactions$).toBeObservable(expected);
+      const expected = cold('--c', { c: completion });
+      expect(effects.loadUserTransactions$).toBeObservable(expected);
     });
   });
 });
-
--- frontend-admin/src/app/transactions/state/transaction.effects.ts
+++ frontend-admin/src/app/transactions/state/transaction.effects.ts
@@ -1,30 +1,32 @@
-import {inject, Injectable} from '@angular/core';
-import {ApiService} from '../../../services/api.service';
-import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {catchError, map, mergeMap} from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, mergeMap } from 'rxjs';
 import {
   loadTransactionListAction,
   loadTransactionListFailure,
-  loadTransactionListSuccess
-} from "./transaction.actions";
-import {HttpErrorResponse} from '@angular/common/http';
+  loadTransactionListSuccess,
+} from './transaction.actions';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class TransactionEffects {
   private readonly _apiService$: ApiService = inject(ApiService);
 
-  selectUserTransactions$ = createEffect(() => {
+  loadUserTransactions$ = createEffect(() => {
     return inject(Actions).pipe(
       ofType(loadTransactionListAction),
       mergeMap((action) => {
-        return this._apiService$.loadTransactionList(action.selectTransactionRequest).pipe(
-          map((transactions) => {
-            return loadTransactionListSuccess({transactions});
-          }),
-          catchError((errorResponse: HttpErrorResponse) => {
-            return [loadTransactionListFailure({errorResponse})];
-          }),
-        );
+        return this._apiService$
+          .loadTransactionList(action.selectTransactionRequest)
+          .pipe(
+            map((transactions) => {
+              return loadTransactionListSuccess({ transactions });
+            }),
+            catchError((errorResponse: HttpErrorResponse) => {
+              return [loadTransactionListFailure({ errorResponse })];
+            }),
+          );
       }),
     );
   });
