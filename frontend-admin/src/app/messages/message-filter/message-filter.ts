210c283c2537e6ba231770885024d1a05b22ee86
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-16 09:29:17 +0200

    load messages from database

--- frontend-admin/src/app/messages/message-filter/message-filter.ts
+++ frontend-admin/src/app/messages/message-filter/message-filter.ts
@@ -1,37 +1,58 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
-  Validators,
+  Validators
 } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
-import { Store } from '@ngrx/store';
-import { MessageState, selectSystemMessages } from '../state/message.selectors';
-import { SystemMessage } from '../../api/model/systemMessage';
+import { BsDatepickerDirective, BsDatepickerInputDirective } from 'ngx-bootstrap/datepicker';
+import { MessageFilterParameters } from '../message-filter-parameters';
 
 @Component({
   selector: 'app-message-filter',
   templateUrl: './message-filter.html',
   styleUrl: './message-filter.css',
-  imports: [FormsModule, ReactiveFormsModule, TranslatePipe],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    TranslatePipe,
+    BsDatepickerDirective,
+    BsDatepickerInputDirective,
+  ],
 })
-export class MessageFilter implements OnInit {
+export class MessageFilter {
+  @Output() messageRequestEvent = new EventEmitter<MessageFilterParameters>();
   protected readonly formGroup: FormGroup;
-  protected _messages$: SystemMessage[] = [];
+  protected minDateFrom: Date = new Date();
+  protected minDateTo: Date = new Date();
+  protected readonly priorities = [1, 2, 3];
   private readonly formBuilder: FormBuilder = inject(FormBuilder);
-  private readonly _storeMessages$: Store<MessageState> = inject(Store);
 
   constructor() {
     this.formGroup = this.formBuilder.group({
-      priority: [null, Validators.required],
+      priority: new FormControl(null, [Validators.required]),
+      dateFrom: new FormControl('', [Validators.required]),
+      dateTo: new FormControl('', [Validators.required]),
     });
   }
 
-  ngOnInit() {
-    this._storeMessages$.select(selectSystemMessages).subscribe((messages) => {
-      this._messages$ = messages;
-    });
+  updateDateToMinDate(value: Date) {
+    const nextDay: Date = new Date(value);
+    nextDay.setDate(nextDay.getDate() + 1);
+    this.minDateTo = nextDay;
+    this.formGroup.patchValue({ dateTo: '' });
+  }
+
+  loadMessageList() {
+    const messageFilterParameters = {
+      dateFrom: this.formGroup.get('dateFrom')?.value,
+      dateTo: this.formGroup.get('dateTo')?.value,
+      priority: this.formGroup.get('priority')?.value,
+    } as MessageFilterParameters;
+
+    this.messageRequestEvent.emit(messageFilterParameters);
   }
 }
--- frontend-admin/src/app/messages/message-list-form/message-list-form.ts
+++ frontend-admin/src/app/messages/message-list-form/message-list-form.ts
@@ -3,6 +3,7 @@ import { MenuComponent } from '../../menu/menu.component';
 import { MessageMenu } from '../message-menu/message-menu';
 import { MessageFilter } from '../message-filter/message-filter';
 import { MessageList } from '../message-list/message-list';
+import { MessageFilterParameters } from '../message-filter-parameters';
 
 @Component({
   selector: 'app-message-list-form',
@@ -10,4 +11,9 @@ import { MessageList } from '../message-list/message-list';
   styleUrl: './message-list-form.css',
   imports: [MenuComponent, MessageMenu, MessageFilter, MessageList],
 })
-export class MessageListForm {}
+export class MessageListForm {
+  protected searchParams: MessageFilterParameters | undefined = undefined;
+  onMessageFilterChanged($event: MessageFilterParameters) {
+    this.searchParams = $event;
+  }
+}
