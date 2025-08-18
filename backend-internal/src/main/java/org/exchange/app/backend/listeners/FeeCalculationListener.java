acc77362b14698a211dac3837af1f1ae16dd4e9e
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-18 14:08:29 +0200

    test for FeeCalculationListener

--- backend-internal/src/main/java/org/exchange/app/backend/listeners/FeeCalculationListener.java
+++ backend-internal/src/main/java/org/exchange/app/backend/listeners/FeeCalculationListener.java
@@ -2,20 +2,23 @@ package org.exchange.app.backend.listeners;
 
 import static org.exchange.app.backend.db.specifications.ExchangeEventSourceSpecification.eventId;
 import static org.exchange.app.backend.db.specifications.ExchangeEventSourceSpecification.eventType;
+import static org.springframework.util.StringUtils.hasText;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
-import org.exchange.app.backend.common.builders.CoreTicket;
 import org.exchange.app.backend.common.config.KafkaConfig;
 import org.exchange.app.backend.common.config.KafkaConfig.Deserializers;
 import org.exchange.app.backend.common.config.KafkaConfig.TopicToInternalBackend;
+import org.exchange.app.backend.common.config.SystemConfig;
+import org.exchange.app.backend.common.exceptions.ObjectWithIdNotFoundException;
 import org.exchange.app.backend.common.utils.ExchangeDateUtils;
 import org.exchange.app.backend.db.entities.ExchangeEventSourceEntity;
 import org.exchange.app.backend.db.repositories.ExchangeEventSourceRepository;
 import org.exchange.app.backend.db.repositories.UserAccountRepository;
 import org.exchange.app.backend.db.utils.ChecksumUtil;
+import org.exchange.app.common.api.model.Currency;
 import org.exchange.app.common.api.model.EventType;
 import org.exchange.internal.app.core.strategies.fee.FeeCalculationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,10 +58,11 @@ public class FeeCalculationListener {
   @KafkaHandler
   public void listen(@Payload String ticket) {
     log.info("*** Received fee messages {}", ticket);
-    Long ticketId = Long.valueOf(ticket);
-    if (ticketId == null) {
+    if (!hasText(ticket)) {
       return;
     }
+    Long ticketId = Long.valueOf(ticket);
+
     try {
 
       List<ExchangeEventSourceEntity> list = exchangeEventSourceRepository.findAll(
@@ -68,14 +72,14 @@ public class FeeCalculationListener {
       );
       if (!list.isEmpty()) {
         long amountSum = list.stream().mapToLong(ExchangeEventSourceEntity::getAmount).sum();
-        UUID userAccountId = list.getFirst().getUserAccountId();
         String currency = list.getFirst().getCurrency();
+        UUID systemAccountId = getSystemAccount(currency);
 
         ExchangeEventSourceEntity entity = new ExchangeEventSourceEntity();
         entity.setEventType(EventType.FEE);
         entity.setDateUtc(ExchangeDateUtils.currentLocalDateTime());
         entity.setAmount(feeCalculationStrategy.calculateFee(amountSum));
-        entity.setUserAccountId(userAccountId);
+        entity.setUserAccountId(systemAccountId);
         entity.setCurrency(currency);
         entity.setEventId(ticketId);
         entity.setChecksum(ChecksumUtil.checksum(entity));
@@ -87,16 +91,11 @@ public class FeeCalculationListener {
     }
   }
 
-  private long getExchangedAmount(CoreTicket coreTicket) {
-    return exchangeEventSourceRepository
-        .findAll(
-            Specification.allOf(
-                eventId(coreTicket.getId()),
-                eventType(EventType.EXCHANGE)
-            )
-        )
-        .stream()
-        .mapToLong(ExchangeEventSourceEntity::getAmount)
-        .sum();
+  private UUID getSystemAccount(String currency) {
+    return userAccountRepository.findByUserIdAndCurrency(SystemConfig.systemUserId,
+            Currency.fromValue(currency))
+        .orElseThrow(
+            () -> new ObjectWithIdNotFoundException("System account", currency)
+        ).getId();
   }
 }
---
+++ backend-internal/src/test/java/org/exchange/app/backend/listeners/FeeCalculationListenerTest.java
@@ -0,0 +1,75 @@
+package org.exchange.app.backend.listeners;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.exchange.app.backend.db.specifications.ExchangeEventSourceSpecification.eventId;
+import static org.exchange.app.backend.listeners.ExchangeResultTicketListenerTest.REAL_USER_1;
+import static org.exchange.app.backend.listeners.ExchangeResultTicketListenerTest.REAL_USER_ACCOUNT_EUR;
+import static org.exchange.app.backend.listeners.ExchangeResultTicketListenerTest.REAL_USER_ACCOUNT_PLN;
+
+import java.util.List;
+import java.util.UUID;
+import org.exchange.app.backend.common.utils.ExchangeDateUtils;
+import org.exchange.app.backend.db.entities.ExchangeEventEntity;
+import org.exchange.app.backend.db.entities.ExchangeEventSourceEntity;
+import org.exchange.app.backend.db.repositories.ExchangeEventRepository;
+import org.exchange.app.backend.db.repositories.ExchangeEventSourceRepository;
+import org.exchange.app.backend.db.utils.ChecksumUtil;
+import org.exchange.app.common.api.model.EventType;
+import org.exchange.app.common.api.model.Pair;
+import org.exchange.app.common.api.model.UserTicketStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@EmbeddedKafka(partitions = 1)
+class FeeCalculationListenerTest {
+
+  @Autowired
+  private FeeCalculationListener feeCalculationListener;
+  @Autowired
+  private ExchangeEventSourceRepository exchangeEventSourceRepository;
+  @Autowired
+  private ExchangeEventRepository exchangeEventRepository;
+
+  @Test
+  void listen_should_saveFeeToSystemAccount_when_feeIsCalculated() {
+    ExchangeEventEntity eventEntity = new ExchangeEventEntity();
+    eventEntity.setUserAccountId(UUID.fromString(REAL_USER_ACCOUNT_PLN));
+    eventEntity.setUserId(UUID.fromString(REAL_USER_1));
+    eventEntity.setRatio(4_0000L);
+    eventEntity.setAmount(10_0000L);
+    eventEntity.setDirection("B");
+    eventEntity.setPair(Pair.EUR_PLN);
+    eventEntity.setDateUtc(ExchangeDateUtils.currentTimestamp());
+    eventEntity.setTicketStatus(UserTicketStatus.NEW);
+    eventEntity.setEventType(EventType.ORDER);
+    eventEntity.setAmountRealized(0L);
+    eventEntity = exchangeEventRepository.save(eventEntity);
+    ExchangeEventSourceEntity entity = new ExchangeEventSourceEntity();
+    entity.setAmount(100_0000L);
+    entity.setCurrency("EUR");
+    entity.setEventType(EventType.EXCHANGE);
+    entity.setDateUtc(ExchangeDateUtils.currentLocalDateTime());
+    entity.setUserAccountId(UUID.fromString(REAL_USER_ACCOUNT_EUR));
+    entity.setEventId(eventEntity.getId());
+    entity.setChecksum(ChecksumUtil.checksum(entity));
+    entity = exchangeEventSourceRepository.save(entity);
+    feeCalculationListener.listen(eventEntity.getId().toString());
+
+    List<ExchangeEventSourceEntity> list = exchangeEventSourceRepository.findAll(
+        eventId(eventEntity.getId()));
+
+    ExchangeEventSourceEntity fee = list.stream()
+        .filter(e -> e.getEventType().equals(EventType.FEE)).toList()
+        .getFirst();
+    assertThat(fee).isNotNull();
+    assertThat(fee.getUserAccountId()).isEqualTo(
+        UUID.fromString("8d8a228a-19a4-4f71-9f69-000000000002"));
+    exchangeEventSourceRepository.deleteAll(list);
+    exchangeEventRepository.delete(eventEntity);
+  }
+}
