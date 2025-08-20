95a5e504a4ad430255e13bd9d8d81545c6385624
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-20 10:11:02 +0200

    financial-report.component

--- frontend-client/src/app/reports/financial-report/financial-report.component.ts
+++ frontend-client/src/app/reports/financial-report/financial-report.component.ts
@@ -1,23 +1,74 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ReportMenu } from '../report-menu/report-menu';
 import { MenuComponent } from '../../menu/menu.component';
+import {
+  BsDatepickerConfig,
+  BsDatepickerDirective,
+  BsDatepickerViewMode
+} from 'ngx-bootstrap/datepicker';
+import { FinancialReportRequest } from '../../api';
+import { Store } from '@ngrx/store';
+import { ReportState } from '../state/report.selectors';
+import { loadFinancialReportPdfDocumentAction } from '../state/report.actions';
+import { AccountBalance } from '../../api/model/accountBalance';
+import { AccountState, selectAccountBalanceList } from '../../accounts/state/account.selectors';
+import { loadAccountBalanceListAction } from '../../accounts/state/account.actions';
 
 @Component({
   selector: 'app-financial-report',
-  imports: [ReactiveFormsModule, TranslatePipe, ReportMenu, MenuComponent],
+  imports: [ReactiveFormsModule, TranslatePipe, ReportMenu, MenuComponent, BsDatepickerDirective],
   templateUrl: './financial-report.component.html',
   styleUrl: './financial-report.component.css'
 })
-export class FinancialReportComponent {
+export class FinancialReportComponent implements OnInit {
   protected formGroup: FormGroup;
+  protected maxDate: Date = new Date();
   private formBuilder: FormBuilder = inject(FormBuilder);
+  protected bsConfig?: Partial<BsDatepickerConfig>;
+  minMode: BsDatepickerViewMode = 'month';
+  private _storeReports$: Store<ReportState> = inject(Store);
+  protected _account$: AccountBalance[] = [];
+  private _storeAccount$: Store<AccountState> = inject(Store);
 
   constructor() {
-    this.formGroup = this.formBuilder.group({});
+    this.formGroup = this.formBuilder.group({
+      month: [null, Validators.required],
+      year: [null, Validators.required],
+      yearAndMonth: [null],
+      accountId: [null, Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.bsConfig = Object.assign({}, {
+      minMode: this.minMode,
+      maxDate: new Date()
+    });
+    this._storeAccount$
+    .select(selectAccountBalanceList)
+    .subscribe((data: AccountBalance[]) => {
+      this._account$ = data;
+    });
+    this._storeAccount$.dispatch(loadAccountBalanceListAction());
   }
 
   generateFinancialReport() {
+    const financialReportRequest: FinancialReportRequest = {
+      month: this.formGroup.get('month')?.value,
+      year: this.formGroup.get('year')?.value,
+      userAccountIDs: [this.formGroup.get('accountId')?.value]
+    };
+    this._storeReports$.dispatch(loadFinancialReportPdfDocumentAction({ financialReportRequest }));
+  }
+
+  updateMonth(value: Date) {
+    let dateString: string = value.toISOString();
+    this.formGroup.patchValue({
+      year: dateString.substring(0, 4),
+      month: dateString.substring(6, 7),
+      yearAndMonth: dateString.substring(0, 7)
+    });
   }
 }
--- frontend-client/src/app/reports/reports-routing.module.ts
+++ frontend-client/src/app/reports/reports-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { provideEffects } from '@ngrx/effects';
+import { EffectsModule, provideEffects } from '@ngrx/effects';
 import { TicketEffects } from '../tickets/state/ticket.effects';
 import { AccountEffects } from '../accounts/state/account.effects';
 import { FinancialReportComponent } from './financial-report/financial-report.component';
 import { canActivateAuthRole } from '../../services/auth-guard/auth-guard.service';
 import { ReportsComponent } from './reports.component';
+import { StoreModule } from '@ngrx/store';
+import { Features } from '../features';
+import { reportsReducers } from './state/report.reducers';
+import { ReportEffects } from './state/report.effects';
+import { accountReducers } from '../accounts/state/account.reducers';
 
 const routes: Routes = [
   {
@@ -26,9 +31,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forChild(routes)
-    //StoreModule.forFeature(Features.reports, reportsReducers),
-    //EffectsModule.forFeature([ReportEffects])
+    RouterModule.forChild(routes),
+    StoreModule.forFeature(Features.reports, reportsReducers),
+    StoreModule.forFeature(Features.accounts, accountReducers),
+    EffectsModule.forFeature([ReportEffects])
   ],
   exports: [RouterModule]
 })
