5de60070379a82c8ee00916f93ffd0ffe6921700
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-21 15:02:52 +0200

    DirectionUtils for null values

--- backend-common/src/main/java/org/exchange/app/backend/common/utils/DirectionUtils.java
+++ backend-common/src/main/java/org/exchange/app/backend/common/utils/DirectionUtils.java
@@ -1,14 +1,22 @@
 package org.exchange.app.backend.common.utils;
 
+import static org.exchange.app.backend.common.serializers.PairSerializer.NULL_BYTE;
+
 import org.exchange.app.common.api.model.Direction;
 
 public class DirectionUtils {
 
   public static byte[] directionToByteArray(Direction direction) {
+    if (direction == null) {
+      return new byte[]{NULL_BYTE};
+    }
     return new byte[]{(direction.equals(Direction.BUY)) ? (byte) 1 : (byte) 0};
   }
 
   public static Direction byteArrayToDirection(byte[] bytes) {
+    if (bytes[0] == NULL_BYTE) {
+      return null;
+    }
     if (bytes.length != 1) {
       throw new IllegalArgumentException("Byte array must be exactly 1 byte.");
     }
--- backend-common/src/test/java/org/exchange/app/backend/common/utils/DirectionUtilsTest.java
+++ backend-common/src/test/java/org/exchange/app/backend/common/utils/DirectionUtilsTest.java
@@ -1,6 +1,7 @@
 package org.exchange.app.backend.common.utils;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.exchange.app.backend.common.serializers.PairSerializer.NULL_BYTE;
 
 import org.exchange.app.common.api.model.Direction;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,21 @@ import org.junit.jupiter.api.Test;
 public class DirectionUtilsTest {
 
   @Test
-  public final void byteArrayToDirection() {
+  public final void byteArrayToDirection_should_returnDirection_when_calledWithByteArrayDirectionRepresentation() {
     for (Direction direction : Direction.values()) {
       assertThat(DirectionUtils.byteArrayToDirection(
           DirectionUtils.directionToByteArray(direction))).isEqualTo(
           direction);
     }
+  }
+
+  @Test
+  public final void directionToByteArray_should_returnNULL_BYTE_when_calledWithNullDirection() {
+    assertThat(DirectionUtils.directionToByteArray(null)).isEqualTo(new byte[]{NULL_BYTE});
+  }
+
+  @Test
+  public final void byteArrayToDirection_should_nullDirection_when_calledWithNULL_BYTE() {
+    assertThat(DirectionUtils.byteArrayToDirection(new byte[]{NULL_BYTE})).isNull();
   }
 }
