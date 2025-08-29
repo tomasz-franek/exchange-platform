b6f4da367c3a28a201b8fd5aff0b426dc46fc580
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-29 14:29:57 +0200

    paltform account service

---
+++ backend-db-storage/src/main/java/org/exchange/app/backend/db/services/PlatformAccountService.java
@@ -0,0 +1,10 @@
+package org.exchange.app.backend.db.services;
+
+import java.util.UUID;
+
+public interface PlatformAccountService {
+
+  UUID getExchangeAccountId(String currency);
+
+  UUID getSystemAccountId(String currency);
+}
---
+++ backend-db-storage/src/main/java/org/exchange/app/backend/db/services/PlatformAccountServiceImpl.java
@@ -0,0 +1,50 @@
+package org.exchange.app.backend.db.services;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.exchange.app.backend.common.config.SystemConfig;
+import org.exchange.app.backend.common.exceptions.ObjectWithIdNotFoundException;
+import org.exchange.app.backend.db.repositories.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Service
+public class PlatformAccountServiceImpl implements PlatformAccountService {
+
+  private final Map<String, UUID> exchangeAccountMap;
+  private final Map<String, UUID> systemAccountMap;
+
+
+  public PlatformAccountServiceImpl(@Autowired UserAccountRepository userAccountRepository) {
+    this.exchangeAccountMap = new HashMap<>();
+    userAccountRepository.findByUserId(SystemConfig.exchangeAccountId).forEach(
+        e -> this.exchangeAccountMap.put(e.getCurrency().getCode().toString(), e.getId()));
+    this.systemAccountMap = new HashMap<>();
+    userAccountRepository.findByUserId(SystemConfig.systemAccountId).forEach(
+        e -> this.systemAccountMap.put(e.getCurrency().getCode().toString(), e.getId()));
+  }
+
+  @Override
+  public UUID getExchangeAccountId(String currency) {
+
+    UUID exchangeAccountId = this.exchangeAccountMap.getOrDefault(currency, null);
+    if (exchangeAccountId == null) {
+      throw new ObjectWithIdNotFoundException("ExchangeAccountId", currency);
+    }
+    return exchangeAccountId;
+  }
+
+  @Override
+  public UUID getSystemAccountId(String currency) {
+
+    UUID systemAccountId = this.systemAccountMap.getOrDefault(currency, null);
+    if (systemAccountId == null) {
+      throw new ObjectWithIdNotFoundException("SystemAccountId", currency);
+    }
+    return systemAccountId;
+  }
+
+}
