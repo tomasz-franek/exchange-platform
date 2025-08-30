631063488bda52d32dfef9210a9f87596d9fccaa
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-08-30 09:47:16 +0200

    remove amount from user account and move it to system account when fee calculated

--- backend-internal/src/main/java/org/exchange/app/backend/listeners/FeeCalculationListener.java
+++ backend-internal/src/main/java/org/exchange/app/backend/listeners/FeeCalculationListener.java
@@ -4,7 +4,7 @@ import static org.exchange.app.backend.db.specifications.ExchangeEventSourceSpec
 import static org.exchange.app.backend.db.specifications.ExchangeEventSourceSpecification.eventType;
 import static org.springframework.util.StringUtils.hasText;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
@@ -12,13 +12,11 @@ import org.exchange.app.backend.common.config.KafkaConfig;
 import org.exchange.app.backend.common.config.KafkaConfig.Deserializers;
 import org.exchange.app.backend.common.config.KafkaConfig.TopicToInternalBackend;
 import org.exchange.app.backend.common.config.SystemConfig;
-import org.exchange.app.backend.common.exceptions.ObjectWithIdNotFoundException;
 import org.exchange.app.backend.common.utils.ExchangeDateUtils;
 import org.exchange.app.backend.db.entities.ExchangeEventSourceEntity;
 import org.exchange.app.backend.db.repositories.ExchangeEventSourceRepository;
-import org.exchange.app.backend.db.repositories.UserAccountRepository;
+import org.exchange.app.backend.db.services.PlatformAccountService;
 import org.exchange.app.backend.db.utils.ChecksumUtil;
-import org.exchange.app.common.api.model.Currency;
 import org.exchange.app.common.api.model.EventType;
 import org.exchange.internal.app.core.strategies.fee.FeeCalculationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,16 +41,15 @@ public class FeeCalculationListener {
 
   private final FeeCalculationStrategy feeCalculationStrategy;
   private final ExchangeEventSourceRepository exchangeEventSourceRepository;
-  private final UserAccountRepository userAccountRepository;
-  private final ObjectMapper objectMapper = new ObjectMapper();
+  private final PlatformAccountService platformAccountService;
 
   @Autowired
   FeeCalculationListener(FeeCalculationStrategy feeCalculationStrategy,
       ExchangeEventSourceRepository exchangeEventSourceRepository,
-      UserAccountRepository userAccountRepository) {
+      PlatformAccountService platformAccountService) {
     this.feeCalculationStrategy = feeCalculationStrategy;
     this.exchangeEventSourceRepository = exchangeEventSourceRepository;
-    this.userAccountRepository = userAccountRepository;
+    this.platformAccountService = platformAccountService;
   }
 
   @KafkaHandler
@@ -71,33 +68,40 @@ public class FeeCalculationListener {
               eventType(EventType.EXCHANGE))
       );
       if (!list.isEmpty()) {
-        long amountSum = list.stream().mapToLong(ExchangeEventSourceEntity::getAmount).sum();
+        long amountSum = list.stream()
+            .filter(e -> !platformAccountService.systemAccountIdsContain(e.getUserAccountId()))
+            .mapToLong(ExchangeEventSourceEntity::getAmount).sum();
         String currency = list.getFirst().getCurrency();
-        UUID systemAccountId = getSystemAccount(currency);
+        UUID systemAccountId = platformAccountService.getSystemAccountId(currency);
+        long amount = feeCalculationStrategy.calculateFee(amountSum);
+        LocalDateTime currentDate = ExchangeDateUtils.currentLocalDateTime();
 
         ExchangeEventSourceEntity entity = new ExchangeEventSourceEntity();
         entity.setEventType(EventType.FEE);
-        entity.setDateUtc(ExchangeDateUtils.currentLocalDateTime());
-        entity.setAmount(feeCalculationStrategy.calculateFee(amountSum));
-        entity.setUserAccountId(systemAccountId);
+        entity.setDateUtc(currentDate);
+        entity.setAmount(-amount);
+        entity.setUserAccountId(list.getFirst().getUserAccountId());
         entity.setCurrency(currency);
         entity.setEventId(ticketId);
         entity.setCreatedBy(SystemConfig.systemAccountId);
-        entity.setCreatedDateUtc(ExchangeDateUtils.currentLocalDateTime());
+        entity.setCreatedDateUtc(currentDate);
         entity.setChecksum(ChecksumUtil.checksum(entity));
-        exchangeEventSourceRepository.save(entity);
+
+        ExchangeEventSourceEntity exchangeEntity = new ExchangeEventSourceEntity();
+        exchangeEntity.setEventType(EventType.FEE);
+        exchangeEntity.setDateUtc(currentDate);
+        exchangeEntity.setAmount(amount);
+        exchangeEntity.setUserAccountId(systemAccountId);
+        exchangeEntity.setCurrency(currency);
+        exchangeEntity.setEventId(ticketId);
+        exchangeEntity.setCreatedBy(SystemConfig.systemAccountId);
+        exchangeEntity.setCreatedDateUtc(currentDate);
+        exchangeEntity.setChecksum(ChecksumUtil.checksum(exchangeEntity));
+        exchangeEventSourceRepository.saveAll(List.of(entity, exchangeEntity));
       }
     } catch (Exception e) {
       throw new RuntimeException(
           "Unable to add Fee to ExchangeEventSource table ", e);
     }
-  }
-
-  private UUID getSystemAccount(String currency) {
-    return userAccountRepository.findByUserIdAndCurrency(SystemConfig.systemAccountId,
-            Currency.fromValue(currency))
-        .orElseThrow(
-            () -> new ObjectWithIdNotFoundException("System account", currency)
-        ).getId();
   }
 }
