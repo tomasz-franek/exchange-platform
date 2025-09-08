841bbf86edcf77204a0262e76189b00693b2ae02
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-08 16:55:44 +0200

    transaction effects tests

--- frontend-admin/src/app/transactions/state/transaction.selectors.spec.ts
+++ frontend-admin/src/app/transactions/state/transaction.selectors.spec.ts
@@ -1,15 +1,37 @@
-import { selectTransactions, TransactionState } from './transaction.selectors';
-import { Transaction } from '../../api/model/transaction';
+import {
+  selectExchangeTransactions,
+  selectSystemTransactions,
+  selectTransactions,
+  TransactionState,
+} from './transaction.selectors';
 
 describe('Transaction Selectors', () => {
   const mockState: TransactionState = {
     transactions: [
       { dateUtc: '1', amount: 1 },
       { dateUtc: '2', amount: 2 },
-    ] as Transaction[],
+    ],
+    systemTransactions: [
+      { dateUtc: '3', amount: 3 },
+      { dateUtc: '4', amount: 4 },
+    ],
+    exchangeTransactions: [
+      { dateUtc: '5', amount: 5 },
+      { dateUtc: '6', amount: 6 },
+    ],
   };
-  it('should select the accounts report response', () => {
+  it('should select the transactions report response', () => {
     const result = selectTransactions.projector(mockState);
     expect(result).toEqual(mockState.transactions);
+  });
+
+  it('should select the system transactions report response', () => {
+    const result = selectSystemTransactions.projector(mockState);
+    expect(result).toEqual(mockState.systemTransactions);
+  });
+
+  it('should select the exchange transactions report response', () => {
+    const result = selectExchangeTransactions.projector(mockState);
+    expect(result).toEqual(mockState.exchangeTransactions);
   });
 });
