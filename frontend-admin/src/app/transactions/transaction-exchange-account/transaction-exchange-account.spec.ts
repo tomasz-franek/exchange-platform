0deb3f627df0b123d681c94de2d073f22e44f9f1
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-08 19:36:37 +0200

    rename fee accounts to exchange accounts

--- frontend-admin/src/app/transactions/transaction-exchange-account/transaction-exchange-account.spec.ts
+++ frontend-admin/src/app/transactions/transaction-exchange-account/transaction-exchange-account.spec.ts
@@ -1,8 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TransactionFeeAccount } from './transaction-fee-account';
+import { TransactionExchangeAccount } from './transaction-exchange-account';
 import { MenuComponent } from '../../menu/menu.component';
-import { testComponentTranslation, testTranslations } from '../../../mocks/test-functions';
+import {
+  testComponentTranslation,
+  testTranslations,
+} from '../../../mocks/test-functions';
 import Keycloak from 'keycloak-js';
 import { MockKeycloak } from '../../../mocks/mock-keycloak';
 import { KEYCLOAK_EVENT_SIGNAL } from 'keycloak-angular';
@@ -12,13 +15,13 @@ import { initialTransactionState } from '../state/transaction.reducers';
 import { ActivatedRoute } from '@angular/router';
 import { mockRoute } from '../../../mocks/activated-route-mock';
 
-describe('TransactionFeeAccount', () => {
-  let component: TransactionFeeAccount;
-  let fixture: ComponentFixture<TransactionFeeAccount>;
+describe('TransactionExchangeAccount', () => {
+  let component: TransactionExchangeAccount;
+  let fixture: ComponentFixture<TransactionExchangeAccount>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TransactionFeeAccount, MenuComponent, testTranslations()],
+      imports: [TransactionExchangeAccount, MenuComponent, testTranslations()],
       providers: [
         { provide: Keycloak, useClass: MockKeycloak },
         {
@@ -30,7 +33,7 @@ describe('TransactionFeeAccount', () => {
       ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TransactionFeeAccount);
+    fixture = TestBed.createComponent(TransactionExchangeAccount);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
