5dffa7bc38db0d8919b5d497dc8456c093493dcc
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-08 16:55:33 +0200

    transaction effects load exchange and system account list

--- frontend-admin/src/app/transactions/state/transaction.actions.ts
+++ frontend-admin/src/app/transactions/state/transaction.actions.ts
@@ -1,7 +1,7 @@
-import {createAction, props} from '@ngrx/store';
-import {HttpErrorResponse} from '@angular/common/http';
-import {Transaction} from '../../api/model/transaction';
-import {SelectTransactionRequest} from '../../api/model/selectTransactionRequest';
+import { createAction, props } from '@ngrx/store';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Transaction } from '../../api/model/transaction';
+import { SelectTransactionRequest } from '../../api/model/selectTransactionRequest';
 
 export const loadTransactionListAction = createAction(
   '[Transaction] Select Transactions',
@@ -13,6 +13,38 @@ export const loadTransactionListSuccess = createAction(
 );
 export const loadTransactionListFailure = createAction(
   '[Transaction] Load Transaction List Failure',
+  props<{
+    errorResponse: HttpErrorResponse;
+  }>(),
+);
+
+export const loadExchangeAccountTransactionListAction = createAction(
+  '[Transaction] Load Exchange Account List',
+  props<{ selectTransactionRequest: SelectTransactionRequest }>(),
+);
+
+export const loadExchangeAccountTransactionListSuccess = createAction(
+  '[Transaction] Load Exchange Account List Success',
+  props<{ exchangeTransactions: Transaction[] }>(),
+);
+export const loadExchangeAccountTransactionListFailure = createAction(
+  '[Transaction] Load Exchange Account List Failure',
+  props<{
+    errorResponse: HttpErrorResponse;
+  }>(),
+);
+
+export const loadSystemAccountTransactionListAction = createAction(
+  '[Transaction] Load System Account List',
+  props<{ selectTransactionRequest: SelectTransactionRequest }>(),
+);
+
+export const loadSystemAccountTransactionListSuccess = createAction(
+  '[Transaction] Load System Account List Success',
+  props<{ systemTransactions: Transaction[] }>(),
+);
+export const loadSystemAccountTransactionListFailure = createAction(
+  '[Transaction] Load System Account List Failure',
   props<{
     errorResponse: HttpErrorResponse;
   }>(),
--- frontend-admin/src/app/transactions/state/transaction.selectors.ts
+++ frontend-admin/src/app/transactions/state/transaction.selectors.ts
@@ -1,17 +1,26 @@
-import {createFeatureSelector, createSelector} from '@ngrx/store';
-import {Features} from '../../features';
-import {Transaction} from "../../api/model/transaction";
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { Features } from '../../features';
+import { Transaction } from '../../api/model/transaction';
 
 export interface TransactionState {
-  transactions: Transaction[]
+  transactions: Transaction[];
+  systemTransactions: Transaction[];
+  exchangeTransactions: Transaction[];
 }
 
-export const selectStatisticFutureState = createFeatureSelector<TransactionState>(
-    Features.transactions,
-);
+export const selectStatisticFutureState =
+  createFeatureSelector<TransactionState>(Features.transactions);
 
 export const selectTransactions = createSelector(
-    selectStatisticFutureState,
-    (state: TransactionState) => state.transactions,
+  selectStatisticFutureState,
+  (state: TransactionState) => state.transactions,
 );
 
+export const selectSystemTransactions = createSelector(
+  selectStatisticFutureState,
+  (state: TransactionState) => state.systemTransactions,
+);
+export const selectExchangeTransactions = createSelector(
+  selectStatisticFutureState,
+  (state: TransactionState) => state.exchangeTransactions,
+);
