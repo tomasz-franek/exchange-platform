f6a9eddd3e0a2312b8d16daf583ec7937825f6b6
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-10 18:58:18 +0200

    statistic menu adaptation

--- frontend-admin/src/app/statistics/statistic-menu/statistic-menu.spec.ts
+++ frontend-admin/src/app/statistics/statistic-menu/statistic-menu.spec.ts
@@ -1,7 +1,20 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {StatisticMenu} from './statistic-menu';
-import {checkMenuChecked, testComponentTranslation, testTranslations,} from '../../../mocks/test-functions';
+import { StatisticMenu } from './statistic-menu';
+import {
+  checkMenuChecked,
+  testComponentTranslation,
+  testTranslations,
+} from '../../../mocks/test-functions';
+import { MenuComponent } from '../../menu/menu.component';
+import Keycloak from 'keycloak-js';
+import { MockKeycloak } from '../../../mocks/mock-keycloak';
+import { KEYCLOAK_EVENT_SIGNAL } from 'keycloak-angular';
+import { MOCK_KEYCLOAK_EVENT_SIGNAL } from '../../../mocks/mock-keycloak-signal';
+import { provideMockStore } from '@ngrx/store/testing';
+import { initialTransactionState } from '../../transactions/state/transaction.reducers';
+import { ActivatedRoute } from '@angular/router';
+import { mockRoute } from '../../../mocks/activated-route-mock';
 
 describe('StatisticMenu', () => {
   let component: StatisticMenu;
@@ -9,7 +22,16 @@ describe('StatisticMenu', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StatisticMenu, testTranslations()],
+      imports: [StatisticMenu, MenuComponent, testTranslations()],
+      providers: [
+        { provide: Keycloak, useClass: MockKeycloak },
+        {
+          provide: KEYCLOAK_EVENT_SIGNAL,
+          useValue: MOCK_KEYCLOAK_EVENT_SIGNAL,
+        },
+        provideMockStore({ initialState: initialTransactionState }),
+        { provide: ActivatedRoute, useValue: mockRoute },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StatisticMenu);
@@ -22,16 +44,28 @@ describe('StatisticMenu', () => {
   });
 
   it('should render page in english (default)', () => {
-    testComponentTranslation(fixture, 'en', '#labelStatisticTransactions', 'Transactions');
+    testComponentTranslation(
+      fixture,
+      'en',
+      '#labelStatisticTransactions',
+      'Transactions',
+    );
   });
 
   it('should render page in proper language', () => {
-    testComponentTranslation(fixture, 'pl', '#labelStatisticTransactions', 'Transakcje');
+    testComponentTranslation(
+      fixture,
+      'pl',
+      '#labelStatisticTransactions',
+      'Transakcje',
+    );
   });
 
   [
-    {id: 'statisticTransactions', description: 'Statistic Transactions'},
-  ].forEach(({id, description}) => {
+    { id: 'statisticsTransaction', description: 'Statistic Transactions' },
+    { id: 'statisticCurrency', description: 'Statistic Currency' },
+    { id: 'statisticsPair', description: 'Statistic Pair' },
+  ].forEach(({ id, description }) => {
     it(`should check the menu option ${description} when clicked`, () => {
       checkMenuChecked(fixture, `#${id}`);
     });
--- frontend-admin/src/app/statistics/statistics.module.ts
+++ frontend-admin/src/app/statistics/statistics.module.ts
@@ -1,16 +1,20 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {StatisticsRoutingModule} from './statistics-routing.module';
-import {StatisticsComponent} from './statistics.component';
-
+import { StatisticsRoutingModule } from './statistics-routing.module';
+import { StatisticsComponent } from './statistics.component';
+import { StatisticPair } from './statistic-pair/statistic-pair';
+import { StatisticCurrency } from './statistic-currency/statistic-currency';
+import { StatisticTransaction } from './statistic-transaction/statistic-transaction';
 
 @NgModule({
   imports: [
     CommonModule,
     StatisticsRoutingModule,
-    StatisticsComponent
-  ]
+    StatisticsComponent,
+    StatisticTransaction,
+    StatisticPair,
+    StatisticCurrency,
+  ],
 })
-export class StatisticsModule {
-}
+export class StatisticsModule {}
