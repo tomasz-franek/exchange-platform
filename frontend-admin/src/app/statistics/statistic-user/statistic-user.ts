1e63e934f6af3df9a39ed12243013d0429aefe5f
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-11 22:43:18 +0200

    StatisticUser component

---
+++ frontend-admin/src/app/statistics/statistic-user/statistic-user.ts
@@ -0,0 +1,35 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  selectUsersStatisticResponse,
+  StatisticState,
+} from '../state/statistic.selectors';
+import { loadUserStatisticAction } from '../state/statistic.actions';
+import { UsersStatisticRequest } from '../../api/model/usersStatisticRequest';
+import { UsersStatisticResponse } from '../../api/model/usersStatisticResponse';
+import { MenuComponent } from '../../menu/menu.component';
+import { StatisticMenu } from '../statistic-menu/statistic-menu';
+import { TranslatePipe } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-statistic-user',
+  templateUrl: './statistic-user.html',
+  styleUrl: './statistic-user.css',
+  imports: [MenuComponent, StatisticMenu, TranslatePipe],
+})
+export class StatisticUser implements OnInit {
+  protected usersStatisticResponse: UsersStatisticResponse | null = null;
+  private _storeProperty$: Store<StatisticState> = inject(Store);
+
+  ngOnInit() {
+    this._storeProperty$
+      .select(selectUsersStatisticResponse)
+      .subscribe((usersStatisticResponse) => {
+        this.usersStatisticResponse = usersStatisticResponse;
+      });
+    const usersStatisticRequest: UsersStatisticRequest = { userId: '' };
+    this._storeProperty$.dispatch(
+      loadUserStatisticAction({ usersStatisticRequest }),
+    );
+  }
+}
