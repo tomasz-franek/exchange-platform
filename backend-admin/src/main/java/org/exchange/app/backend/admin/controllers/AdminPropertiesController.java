3259214b067019214c4d052d1a4deca21742601d
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-11 21:55:18 +0200

    AdminPropertiesController + API endpoint

---
+++ backend-admin/src/main/java/org/exchange/app/backend/admin/controllers/AdminPropertiesController.java
@@ -0,0 +1,22 @@
+package org.exchange.app.backend.admin.controllers;
+
+import org.exchange.app.admin.api.PropertiesApi;
+import org.exchange.app.admin.api.model.SystemPropertyResponse;
+import org.exchange.app.backend.admin.services.AdminPropertiesService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminPropertiesController implements PropertiesApi {
+
+  private final AdminPropertiesService adminPropertiesService;
+
+  public AdminPropertiesController(AdminPropertiesService adminPropertiesService) {
+    this.adminPropertiesService = adminPropertiesService;
+  }
+
+  @Override
+  public ResponseEntity<SystemPropertyResponse> loadSystemProperties() {
+    return ResponseEntity.ok(adminPropertiesService.loadSystemProperties());
+  }
+}
