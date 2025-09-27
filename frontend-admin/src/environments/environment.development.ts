7359806d9aa903c724e53d88467eb20bc466c9fb
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-27 14:05:03 +0200

    build prod for admin UI

--- frontend-admin/src/environments/environment.development.ts
+++ frontend-admin/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  BASE_PATH: 'http://localhost:8090',
   ADMIN_BASE_PATH: 'http://localhost:8090',
   INTERNAL_BASE_PATH: 'http://localhost:8002',
   EXTERNAL_BASE_PATH: 'http://localhost:8080',
--- frontend-admin/src/environments/environment.production.ts
+++ frontend-admin/src/environments/environment.production.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true,
+  BASE_PATH: 'http://localhost:8090',
+  ADMIN_BASE_PATH: 'http://localhost:8090',
+  INTERNAL_BASE_PATH: 'http://localhost:8002',
+  EXTERNAL_BASE_PATH: 'http://localhost:8080',
 };
