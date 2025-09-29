5d92f62c496ce72d5311d42822756365b2262929
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-09-29 21:28:58 +0200

    added cache for rates service

--- backend-common/src/main/java/org/exchange/app/backend/common/cache/CacheConfiguration.java
+++ backend-common/src/main/java/org/exchange/app/backend/common/cache/CacheConfiguration.java
@@ -4,5 +4,5 @@ public class CacheConfiguration {
 
   public static final String KEYCLOAK_TOKEN_CACHE = "keycloak-token-cache";
   public static final String USER_ACCOUNT_CURRENCY_CACHE = "user-account-currency-cache";
-
+  public static final String RATES_CACHE = "rates-cache";
 }
--- backend-external/src/main/java/org/exchange/app/backend/external/services/RatesServiceImpl.java
+++ backend-external/src/main/java/org/exchange/app/backend/external/services/RatesServiceImpl.java
@@ -1,29 +1,46 @@
 package org.exchange.app.backend.external.services;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.time.Duration;
 import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.exchange.app.backend.common.cache.CacheConfiguration;
 import org.exchange.app.common.api.model.Pair;
 import org.exchange.app.external.api.model.CurrencyRate;
+import org.springframework.cache.Cache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 public class RatesServiceImpl implements RatesService {
 
+  private final Cache ratesCache;
+
+  public RatesServiceImpl() {
+    CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+    cacheManager.registerCustomCache(CacheConfiguration.RATES_CACHE,
+        Caffeine.newBuilder().expireAfterAccess(Duration.ofSeconds(5)).build());
+    this.ratesCache = cacheManager.getCache(CacheConfiguration.RATES_CACHE);
+  }
 
   // todo implement
   @Override
   public List<CurrencyRate> loadCurrencyRates() {
-    CurrencyRate currencyRateEurUsd = new CurrencyRate();
-    currencyRateEurUsd.setPair(Pair.EUR_USD);
-    currencyRateEurUsd.sellAmount(100L);
-    currencyRateEurUsd.buyAmount(200L);
-    currencyRateEurUsd.setBuyRate(1_0000L);
-    currencyRateEurUsd.setSellRate(1_1000L);
-    CurrencyRate currencyRateEurGbp = new CurrencyRate();
-    currencyRateEurGbp.setPair(Pair.EUR_GBP);
-    currencyRateEurGbp.sellAmount(500L);
-    currencyRateEurGbp.buyAmount(800L);
-    currencyRateEurGbp.setBuyRate(1_0800L);
-    currencyRateEurGbp.setSellRate(1_0920L);
-    return List.of(currencyRateEurUsd, currencyRateEurGbp);
+    return this.ratesCache.get(CacheConfiguration.RATES_CACHE, () -> {
+      CurrencyRate currencyRateEurUsd = new CurrencyRate();
+      currencyRateEurUsd.setPair(Pair.EUR_USD);
+      currencyRateEurUsd.sellAmount(100L);
+      currencyRateEurUsd.buyAmount(200L);
+      currencyRateEurUsd.setBuyRate(1_0000L);
+      currencyRateEurUsd.setSellRate(1_1000L);
+      CurrencyRate currencyRateEurGbp = new CurrencyRate();
+      currencyRateEurGbp.setPair(Pair.EUR_GBP);
+      currencyRateEurGbp.sellAmount(500L);
+      currencyRateEurGbp.buyAmount(800L);
+      currencyRateEurGbp.setBuyRate(1_0800L);
+      currencyRateEurGbp.setSellRate(1_0920L);
+      return List.of(currencyRateEurUsd, currencyRateEurGbp);
+    });
   }
 }
