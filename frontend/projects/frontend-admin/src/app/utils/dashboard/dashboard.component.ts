297a1421cb3d94beb4b6755b01fa3e910f572237
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-12-06 09:34:41 +0100

    dashboard page with currency statuses - initial version

--- frontend/projects/frontend-admin/src/app/utils/dashboard/dashboard.component.ts
+++ frontend/projects/frontend-admin/src/app/utils/dashboard/dashboard.component.ts
@@ -1,20 +1,29 @@
-import {Component, effect, inject, OnInit} from '@angular/core';
-import {TranslatePipe, TranslateService} from '@ngx-translate/core';
-import {FooterComponent} from '../../../../../shared-modules/src/lib/footer/footer.component';
-import {MenuComponent} from '../../menu/menu.component';
-import {PropertyStore} from '../../properties/properties.signal-store';
-import {Toast} from 'primeng/toast';
-import {UtilStore} from '../utils.signal-store';
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { FooterComponent } from '../../../../../shared-modules/src/lib/footer/footer.component';
+import { MenuComponent } from '../../menu/menu.component';
+import { PropertyStore } from '../../properties/properties.signal-store';
+import { Toast } from 'primeng/toast';
+import { UtilStore } from '../utils.signal-store';
+import { CurrencyStatus } from '../../../../../shared-modules/src/lib/currency-status/currency-status';
+import { Pair } from '../../api/model/pair';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TranslatePipe, FooterComponent, MenuComponent, Toast],
+  imports: [
+    TranslatePipe,
+    FooterComponent,
+    MenuComponent,
+    Toast,
+    CurrencyStatus,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
   protected readonly store = inject(UtilStore);
   protected readonly storeProperty = inject(PropertyStore);
+  protected readonly Pair = Pair;
   private translateService: TranslateService = inject(TranslateService);
 
   constructor() {
@@ -24,12 +33,10 @@ export class DashboardComponent implements OnInit {
       if (userProperty && userProperty.language != undefined) {
         this.translateService.use(userProperty.language).pipe().subscribe();
       }
-    })
+    });
   }
 
   ngOnInit() {
     this.storeProperty.getUserProperty();
-
-
   }
 }
--- frontend/projects/frontend-admin/src/app/utils/landing-page/landing-page.component.ts
+++ frontend/projects/frontend-admin/src/app/utils/landing-page/landing-page.component.ts
@@ -1,17 +1,14 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {TranslatePipe} from '@ngx-translate/core';
-import {FooterComponent} from '../../../../../shared-modules/src/lib/footer/footer.component';
-import {UtilStore} from '../utils.signal-store';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { FooterComponent } from '../../../../../shared-modules/src/lib/footer/footer.component';
+import { UtilStore } from '../utils.signal-store';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  imports: [
-    TranslatePipe,
-    FooterComponent
-  ],
-  styleUrl: './landing-page.component.scss'
+  imports: [TranslatePipe, FooterComponent],
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent implements OnInit {
   protected readonly router: Router = inject(Router);
