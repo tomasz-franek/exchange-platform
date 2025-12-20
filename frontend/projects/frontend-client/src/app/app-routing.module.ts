b59a6e42c6c8ace8c4ee47c600574148ec118c8d
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-12-20 14:11:35 +0100

    remove modularity from angular client

--- frontend/projects/frontend-client/src/app/app-routing.module.ts
+++ frontend/projects/frontend-client/src/app/app-routing.module.ts
@@ -6,6 +6,24 @@ import {ForbiddenComponent} from '../../../shared-modules/src/lib/forbidden/forb
 import {NotFoundComponent} from '../../../shared-modules/src/lib/not-found/not-found.component';
 import {DashboardComponent} from './utils/dashboard/dashboard.component';
 import {LandingPageComponent} from './utils/landing-page/landing-page.component';
+import {TicketsComponent} from './tickets/tickets.component';
+import {TicketListComponent} from './tickets/ticket-list/ticket-list.component';
+import {TicketOrderComponent} from './tickets/ticket-order/ticket-order.component';
+import {TicketRealizedComponent} from './tickets/ticket-realized/ticket-realized.component';
+import {AccountsComponent} from './accounts/accounts.component';
+import {AccountListComponent} from './accounts/account-list/account-list.component';
+import {AccountEditComponent} from './accounts/account-edit/account-edit.component';
+import {AccountWithdrawComponent} from './accounts/account-withdraw/account-withdraw.component';
+import {AccountBankComponent} from './accounts/account-bank/account-bank';
+import {MessageList} from './messages/message-list/message-list';
+import {MessageComponent} from './messages/message.component';
+import {RatesComponent} from './rates/rates.component';
+import {RateList} from './rates/rates-list/rate-list';
+import {ReportsComponent} from './reports/reports.component';
+import {FinancialReportComponent} from './reports/financial-report/financial-report.component';
+import {PropertiesComponent} from './properties/properties.component';
+import {UserPropertyComponent} from './properties/user-property/user-property.component';
+import {PropertyAddressComponent} from './properties/property-address/property-address.component';
 
 export const routes: Routes = [
   {
@@ -22,43 +40,151 @@ export const routes: Routes = [
     path: 'tickets',
     canActivate: [canActivateAuthRole],
     data: {role: 'EXCHANGE_CLIENT'},
-    loadChildren: () =>
-      import('./tickets/tickets.module').then((m) => m.TicketsModule),
+    children: [
+      {
+        path: '',
+        component: TicketsComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'ticket-list',
+        component: TicketListComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'ticket-order',
+        component: TicketOrderComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'ticket-realized',
+        component: TicketRealizedComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      }
+    ]
   },
   {
     path: 'accounts',
     canActivate: [canActivateAuthRole],
     data: {role: 'EXCHANGE_CLIENT'},
-    loadChildren: () =>
-      import('./accounts/accounts.module').then((m) => m.AccountsModule),
+    children: [
+      {
+        path: '',
+        component: AccountsComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'account-list',
+        component: AccountListComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'account-edit',
+        component: AccountEditComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'account-withdraw',
+        component: AccountWithdrawComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'bank-accounts',
+        component: AccountBankComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      }
+    ]
   },
   {
     path: 'messages',
     canActivate: [canActivateAuthRole],
     data: {role: 'EXCHANGE_CLIENT'},
-    loadChildren: () =>
-      import('./messages/messages-module').then((m) => m.MessagesModule),
+    children: [
+      {
+        path: '',
+        component: MessageComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'},
+      },
+      {
+        path: 'message-list',
+        component: MessageList,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'},
+      },
+    ]
   },
   {
     path: 'rates',
     canActivate: [canActivateAuthRole],
     data: {role: 'EXCHANGE_CLIENT'},
-    loadChildren: () =>
-      import('./rates/rates-module').then((m) => m.RatesModule),
+    children: [
+      {
+        path: '',
+        component: RatesComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'},
+      },
+      {
+        path: 'rate-list',
+        component: RateList,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'},
+      },
+    ]
   },
   {
     path: 'reports',
     canActivate: [canActivateAuthRole],
     data: {role: 'EXCHANGE_CLIENT'},
-    loadChildren: () =>
-      import('./reports/reports.module').then((m) => m.ReportsModule),
+    children: [
+      {
+        path: '',
+        component: ReportsComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'financial-report',
+        component: FinancialReportComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      }
+    ]
   },
   {
     path: 'properties',
     canActivate: [canActivateAuthRole],
     data: {role: 'EXCHANGE_CLIENT'},
-    loadChildren: () =>
-      import('./properties/properties.module').then((m) => m.PropertiesModule),
+    children: [
+      {
+        path: '',
+        component: PropertiesComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'user-property',
+        component: UserPropertyComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      },
+      {
+        path: 'property-address',
+        component: PropertyAddressComponent,
+        canActivate: [canActivateAuthRole],
+        data: {role: 'EXCHANGE_CLIENT'}
+      }
+    ]
   },
 
   {
