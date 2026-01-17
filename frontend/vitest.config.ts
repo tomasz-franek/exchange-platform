50b5a4fe865b36bd5dee518a3fab6519eb3018b0
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2026-01-17 06:03:22 +0100

    vitest config CI

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-
 import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig(({ mode }) => ({
@@ -8,13 +7,26 @@ export default defineConfig(({ mode }) => ({
     globals: true,
     setupFiles: ['src/test-setup.ts'],
     environment: 'jsdom',
-    browserMode: false,
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    projects: [
-      'projects/shared-modules',
-      'projects/frontend-admin',
-      'projects/frontend-client',
+    browserMode: true,
+    include: [
+      'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+      './projects/shared-modules/src/lib/**/*.ts',
     ],
+    projects: [
+      './projects/shared-modules',
+      './projects/frontend-admin',
+      './projects/frontend-client',
+    ],
+    references: [
+      './projects/shared-modules/tsconfig.spec.json',
+      './projects/frontend-admin/tsconfig.spec.json',
+      './projects/frontend-client/tsconfig.spec.json',
+    ],
+    resolve: {
+      alias: {
+        'shared-modules': './projects/shared-modules/src/lib',
+      },
+    },
+    reporters: ['default'],
   },
 }));
